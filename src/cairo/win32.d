d39e729614e19b1f7982b7cfbb66af6f5823f8d6
Johannes Pfau <johannespfau@gmail.com>
2011-07-10 14:03:06 +0200

    Add win32 font support

--- src/cairo/win32.d
+++ src/cairo/win32.d
@@ -68,6 +68,82 @@ version(CAIRO_HAS_WIN32_SURFACE)
     }
     version(CAIRO_HAS_WIN32_FONT)
     {
-        //TODO: Font support
+        public class Win32FontFace : FontFace
+        {
+            public:
+                /* Warning: ptr reference count is not increased by this function!
+                 * Adjust reference count before calling it if necessary*/
+                this(cairo_font_face_t* ptr)
+                {
+                    super(ptr);
+                }
+
+                this(LOGFONTW* logfont)
+                {
+                    super(cairo_win32_font_face_create_for_logfontw(logfont));
+                }
+
+                this(HFONT font)
+                {
+                    super(cairo_win32_font_face_create_for_hfont(font));
+                }
+
+                this(LOGFONTW* logfont, HFONT font)
+                {
+                    super(cairo_win32_font_face_create_for_logfontw_hfont(logfont, font));
+                }
+        }
+
+        public class Win32ScaledFont : ScaledFont
+        {
+            public:
+                /* Warning: ptr reference count is not increased by this function!
+                 * Adjust reference count before calling it if necessary*/
+                this(cairo_scaled_font_t* ptr)
+                {
+                    super(ptr);
+                }
+                this(Win32FontFace font_face, Matrix font_matrix, Matrix ctm,
+                    FontOptions options)
+                {
+                    super(font_face, font_matrix, ctm, options);
+                }
+
+                void selectFont(HDC hdc)
+                {
+                    throwError(cairo_win32_scaled_font_select_font(this.nativePointer, hdc));
+                }
+
+                void doneFont()
+                {
+                    cairo_win32_scaled_font_done_font(this.nativePointer);
+                    checkError();
+                }
+
+                double getMetricsFactor()
+                {
+                    auto res = cairo_win32_scaled_font_get_metrics_factor(this.nativePointer);
+                    checkError();
+                    return res;
+                }
+
+                Matrix getLogicalToDevice()
+                {
+                    Matrix mat;
+                    cairo_win32_scaled_font_get_logical_to_device(this.nativePointer,
+                        &mat.nativeMatrix);
+                    checkError();
+                    return mat;
+                }
+
+                Matrix getDeviceToLogical()
+                {
+                    Matrix mat;
+                    cairo_win32_scaled_font_get_device_to_logical(this.nativePointer,
+                        &mat.nativeMatrix);
+                    checkError();
+                    return mat;
+                }
+        }
     }
 }
