ae2a805f76f532d223bc4eff29de3e6f8b86c8af
Johannes Pfau <johannespfau@gmail.com>
2014-03-15 10:00:34 +0100

    Add package.d files

---
+++ src/cairo/c/package.d
@@ -0,0 +1,4 @@
+module cairo.c;
+
+public import cairo.c.cairo, cairo.c.config, cairo.c.directfb, cairo.c.ft, cairo.c.pdf, cairo.c.ps, cairo.c.svg,
+    cairo.c.win32, cairo.c.xcb, cairo.c.xlib;
---
+++ src/cairo/package.d
@@ -0,0 +1,4 @@
+module cairo;
+
+public import cairo.c.config;
+public import cairo.cairo, cairo.directfb, cairo.ft, cairo.pdf, cairo.ps, cairo.svg, cairo.win32, cairo.xcb, cairo.xlib;
