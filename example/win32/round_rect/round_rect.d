6344c59251ed692cb5f5a9d3cc6c6ee3859bb297
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2014-04-08 10:15:29 +0200

    Add more win32 examples.

--- example/win32/round_rect/round_rect.d
+++ example/win32/round_rect/round_rect.d
@@ -1,15 +1,13 @@
-module win32test;
+module round_rect;
 
 /+
  + This tutorial is derived from: http://cairographics.org/cookbook/win32quickstart/
  + Translated to D2 by Andrej Mitrovic, 2011.
- +
- + Assuming that you have the `libcairo-2.lib` import library, the `win32.lib` WindowsAPI library, and the `libcairod.lib` library,
- + compile via: dmd win32test.d libcairo-2.lib -I..\..\src ..\..\libcairod.lib -I..\..\..\WindowsAPI ..\..\..\WindowsAPI\win32.lib -version=Unicode -version=WIN32_WINNT_ONLY -version=WindowsNTonly -version=Windows2000 -version=Windows2003 -version=WindowsXP -version=WindowsVista -version=CAIRO_HAS_PS_SURFACE -version=CAIRO_HAS_PDF_SURFACE -version=CAIRO_HAS_SVG_SURFACE -version=CAIRO_HAS_WIN32_SURFACE -version=CAIRO_HAS_PNG_FUNCTIONS -version=CAIRO_HAS_WIN32_FONT -L-Subsystem:Windows
  +/
 
 import core.runtime;
 import std.utf;
+import std.traits;
 
 pragma(lib, "gdi32.lib");
 import win32.windef;
@@ -17,7 +15,7 @@ import win32.winuser;
 import win32.wingdi;
 
 string appName     = "CairoWindow";
-string description = "A simple win32 window with Cairo drawing";
+string description = "Rounded Rectangles";
 HINSTANCE hinst;
 
 import cairo.cairo;
@@ -25,19 +23,19 @@ import cairo.win32;
 
 alias cairo.cairo.RGB RGB;  // conflicts with win32.wingdi.RGB
 
-static assert(CAIRO_HAS_WIN32_SURFACE);
+import util.rounded_rectangle;
 
 extern (Windows)
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int iCmdShow)
 {
     int result;
-    void exceptionHandler(Throwable e) { throw e; }
+
 
     try
     {
-        Runtime.initialize(&exceptionHandler);
+        Runtime.initialize();
         result = myWinMain(hInstance, hPrevInstance, lpCmdLine, iCmdShow);
-        Runtime.terminate(&exceptionHandler);
+        Runtime.terminate();
     }
     catch (Throwable o)
     {
@@ -61,15 +59,15 @@ int myWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int
     wndclass.cbClsExtra    = 0;
     wndclass.cbWndExtra    = 0;
     wndclass.hInstance     = hInstance;
-    wndclass.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
-    wndclass.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hIcon         = LoadIcon(null, IDI_APPLICATION);
+    wndclass.hCursor       = LoadCursor(null, IDC_ARROW);
     wndclass.hbrBackground = cast(HBRUSH) GetStockObject(WHITE_BRUSH);
     wndclass.lpszMenuName  = appName.toUTF16z;
     wndclass.lpszClassName = appName.toUTF16z;
 
     if (!RegisterClass(&wndclass))
     {
-        MessageBox(NULL, "This program requires Windows NT!", appName.toUTF16z, MB_ICONERROR);
+        MessageBox(null, "This program requires Windows NT!", appName.toUTF16z, MB_ICONERROR);
         return 0;
     }
 
@@ -78,50 +76,23 @@ int myWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int
                         WS_OVERLAPPEDWINDOW,           // window style
                         CW_USEDEFAULT,                 // initial x position
                         CW_USEDEFAULT,                 // initial y position
-                        CW_USEDEFAULT,                 // initial x size
-                        CW_USEDEFAULT,                 // initial y size
-                        NULL,                          // parent window handle
-                        NULL,                          // window menu handle
+                        650,                           // initial x size
+                        250,                           // initial y size
+                        null,                          // parent window handle
+                        null,                          // window menu handle
                         hInstance,                     // program instance handle
-                        NULL);                         // creation parameters
+                        null);                         // creation parameters
 
     ShowWindow(hwnd, iCmdShow);
     UpdateWindow(hwnd);
 
-    while (GetMessage(&msg, NULL, 0, 0))
+    while (GetMessage(&msg, null, 0, 0))
     {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
     return msg.wParam;
-}
-
-void roundedRectangle!double(Context ctx, int x, int y, int w, int h, int radius_x = 5, int radius_y = 5)
-{
-    enum ARC_TO_BEZIER = 0.55228475;
-
-    if (radius_x > w - radius_x)
-        radius_x = w / 2;
-
-    if (radius_y > h - radius_y)
-        radius_y = h / 2;
-
-    // approximate (quite close) the arc using a bezier curve
-    auto c1 = ARC_TO_BEZIER * radius_x;
-    auto c2 = ARC_TO_BEZIER * radius_y;
-
-    ctx.newPath();
-    ctx.moveTo(x + radius_x, y);
-    ctx.relLineTo(w - 2 * radius_x, 0.0);
-    ctx.relCurveTo(c1, 0.0, radius_x, c2, radius_x, radius_y);
-    ctx.relLineTo(0, h - 2 * radius_y);
-    ctx.relCurveTo(0.0, c2, c1 - radius_x, radius_y, -radius_x, radius_y);
-    ctx.relLineTo(-w + 2 * radius_x, 0);
-    ctx.relCurveTo(-c1, 0, -radius_x, -c2, -radius_x, -radius_y);
-    ctx.relLineTo(0, -h + 2 * radius_y);
-    ctx.relCurveTo(0.0, -c2, radius_x - c1, -radius_y, radius_x, -radius_y);
-    ctx.closePath();
 }
 
 extern (Windows)
@@ -134,10 +105,10 @@ LRESULT WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
             window = new Window(hwnd);
             return 0;
         }
-        
+
         default:
     }
-    
+
     if (window)
         return window.process(hwnd, message, wParam, lParam);
     else
@@ -177,15 +148,15 @@ class Window
 
             default:
         }
-        
-        return DefWindowProc(hwnd, message, wParam, lParam);    
+
+        return DefWindowProc(hwnd, message, wParam, lParam);
     }
-    
+
     auto OnPaint(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
     {
         hdc = BeginPaint(hwnd, &ps);
         GetClientRect(hwnd, &rc);
-        
+
         auto left = rc.left;
         auto top = rc.top;
         auto right = rc.right;
@@ -202,27 +173,26 @@ class Window
 
         auto surf = new Win32Surface(_buffer);
         auto ctx = Context(surf);
-        
+
         ctx.setSourceRGB(1, 1, 1);
         ctx.paint();
+        ctx.translate(50, 40);
+        int xPos = 150;
+        int yPos = 10;
 
-        roundedRectangle!double(ctx, 50, 50, 250, 250, 10, 10);
-        
-        auto clr = RGB(0.9411764705882353, 0.996078431372549, 0.9137254901960784);
-        ctx.setSourceRGB(clr);
-        ctx.fillPreserve();
-        
-        clr = RGB(0.7019607843137254, 1.0, 0.5529411764705883);
-        ctx.setSourceRGB(clr);
-        ctx.stroke();
+        foreach (index, method; EnumMembers!RoundMethod)
+        {
+            roundedRectangle!(method)(ctx, index * xPos, yPos, 100, 100, 10, 10);
 
-        ctx.setSourceRGB(0, 0, 0);
-        ctx.selectFontFace("Arial", FontSlant.CAIRO_FONT_SLANT_NORMAL, FontWeight.CAIRO_FONT_WEIGHT_NORMAL);
-        ctx.setFontSize(10.0);
-        auto txt = "Cairo is the greatest thing!";
-        ctx.moveTo(5.0, 10.0);
-        ctx.showText(txt);
-        
+            auto clr = RGB(0.9411764705882353, 0.996078431372549, 0.9137254901960784);
+            ctx.setSourceRGB(clr);
+            ctx.fillPreserve();
+
+            clr = RGB(0.7019607843137254, 1.0, 0.5529411764705883);
+            ctx.setSourceRGB(clr);
+            ctx.stroke();
+        }
+
         surf.finish();
         BitBlt(hdc, 0, 0, width, height, _buffer, x, y, SRCCOPY);
 
