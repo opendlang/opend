dadcef17fba48dbafe1f5e56e030727fa2cd4540
John Colvin <john.loughran.colvin@gmail.com>
2014-07-17 14:32:36 +0100

    change all.d to package.d

--- source/dstats/package.d
+++ source/dstats/package.d
@@ -4,17 +4,9 @@
  * If you think this module is even copyrightable, you have issues.
  */
 
-module dstats.all;
+module dstats;
 
 public import dstats.alloc, dstats.sort, dstats.base, dstats.cor,
               dstats.distrib, dstats.infotheory, dstats.random,
               dstats.summary, dstats.tests, dstats.regress, dstats.kerneldensity,
               dstats.pca;
-
-version(GNU)
-{
-    version(unittest)
-    {
-        void main(){}
-    }
-}
