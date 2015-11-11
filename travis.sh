4eac014ac5eff1379e69b0ec94b9c58d6f468de7
BlackEdder <edder@tkwsping.nl>
2015-11-11 08:05:39 +0000

    Generate documentation on gh-pages

---
+++ travis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+dub test --compiler=${DC}
+
+if [[ $TRAVIS_BRANCH == 'master' ]] ; then
+    if [ ! -z "$GH_TOKEN" ]; then
+        git checkout master
+        dub build -b docs --compiler=${DC}
+        cd docs
+        mkdir images
+        cp ../*.{png,svg} images/
+        git init
+        git config user.name "Travis-CI"
+        git config user.email "travis@nodemeatspace.com"
+        git add .
+        git commit -m "Deployed to Github Pages"
+        git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" HEAD:gh-pages > /dev/null 2>&1
+        #git push --force "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" HEAD:gh-pages
+    fi
+fi
