1b37bc16c0570030fae260a7e013b7d2384e669a
BlackEdder <edder@tkwsping.nl>
2015-11-11 18:02:15 +0000

    Do not copy images

--- travis.sh
+++ travis.sh
@@ -9,8 +9,8 @@ if [[ $TRAVIS_BRANCH == 'master' ]] ; then
         git checkout master
         dub build -b docs --compiler=${DC}
         cd docs
-        mkdir images
-        cp ../*.{png,svg} images/
+        #mkdir images
+        #cp ../*.{png,svg} images/
         git init
         git config user.name "Travis-CI"
         git config user.email "travis@nodemeatspace.com"
