6334021a259b16a90fa46f13468f2077fae6669b
BlackEdder <edder@tkwsping.nl>
2015-12-04 07:44:06 -0500

    Function to calculate the gradient vector
    Copied from my earlier implementation in realtimeplot

--- source/ggplotd/geometry.d
+++ source/ggplotd/geometry.d
@@ -22,7 +22,7 @@ struct Vertex3D
         x = _x; y = _y; z = _z;
     }
 
-    Vertex3D opBinary(string s)( in Vertex3D v2 ) if (s == "-" || s == "+" )
+    Vertex3D opBinary(string s)( in Vertex3D v2 ) const if (s == "-" || s == "+" )
     {
         mixin( "return Vertex3D( x " ~ s ~ " v2.x, y " ~ s ~ 
             " v2.y, z " ~ s ~ " v2.z );");
@@ -54,6 +54,7 @@ unittest
 /// Calculate gradientVector based on a Triangle. The vertices of the triangle are assumed to be sorted by height.
 auto gradientVector( T )( in T triangle )
 {
+    import std.math : pow;
     assert( triangle[0].z <= triangle[1].z && triangle[1].z <= triangle[2].z,
         "gradientVector expects the triangle vertices to be sorted by height" );
     auto gVector = [ Vertex3D(triangle[0]), Vertex3D(triangle[2]) ];
@@ -61,6 +62,29 @@ auto gradientVector( T )( in T triangle )
     if (triangle[0].z == triangle[2].z) {
         return gVector;
     }
+
+    auto e1 = triangle[2]-triangle[0];
+    auto e2 = triangle[1]-triangle[0];
+    auto normal = e1.crossProduct( e2 );
+
+    Vertex3D v;
+    if (normal.x == 0)
+    {
+        v = Vertex3D( normal.x/normal.y, 1, 
+            -(pow(normal.y,2)+pow(normal.x,2))/(normal.y*normal.z)
+            );
+    } else 
+    {
+        v = Vertex3D( 1, normal.y/normal.x, 
+            -(pow(normal.y,2)+pow(normal.x,2))/(normal.x*normal.z)
+            );
+    }
+
+    auto scalar = (gVector[1].z-gVector[0].z)/v.z;
+
+    gVector[1].x = gVector[0].x + scalar*v.x;
+    gVector[1].y = gVector[0].y + scalar*v.y;
+    gVector[1].z = gVector[0].z + scalar*v.z;
 
     return gVector; 
 }
@@ -76,12 +100,14 @@ unittest
     v = gradientVector( [ Vertex3D( 0, 0, 0 ),
         Vertex3D( 0, 1, 0.5 ),
         Vertex3D( 1, 1, 1 )] );
-    v.writeln;
+    assertEqual( v, [Vertex3D(0, 0, 0), Vertex3D(1, 1, 1)] );
 
     v = gradientVector( [ Vertex3D( 0, 0, 0 ),
         Vertex3D( 0, 1, 0.1 ),
         Vertex3D( 1, 1, 1 )] );
-    v.writeln;
- 
+    assertEqual( v[0], Vertex3D(0, 0, 0) );
+    assertApprox( v[1].x, 1.09756, 1e-5 );
+    assertApprox( v[1].y, 0.121951, 1e-5 );
+    assertApprox( v[1].z, 1.0 );
 }
 
