f578cbe331491dd61f44f1f5fad7f2d5221d094e
BlackEdder <edder@tkwsping.nl>
2016-04-22 16:21:09 +0100

    Add statFunction to ggplotd

---
+++ source/ggplotd/stat.d
@@ -0,0 +1,36 @@
+module ggplotd.stat;
+
+version( unittest )
+{
+    import dunit.toolkit;
+}
+
+import std.traits : isCallable;
+
+///
+auto statFunction( FUNC, T )( FUNC func, T min, 
+    T max, size_t precision = 50 ) if (isCallable!FUNC)
+{
+    import std.algorithm : map;
+    import std.range : iota;
+
+    import ggplotd.aes : Aes;
+    auto stepSize = (max-min)/(precision-1);
+    auto xs = iota( min, max+stepSize, stepSize );
+    auto ys = xs.map!((x) => func(x));
+
+    return Aes!(typeof(xs), "x", typeof(ys), "y")( xs, ys );
+}
+
+unittest
+{
+    import std.array : array;
+
+    auto aes = statFunction( (double x) { return 2*x; }, 0.0, 1, 2 );
+    assertEqual( aes.x.array, [0.0, 1.0] );
+    assertEqual( aes.y.array, [0.0, 2.0] );
+
+    aes = statFunction( (double x) { return 3*x; }, -1.0, 1, 3 );
+    assertEqual( aes.x.array, [-1.0, 0.0, 1.0] );
+    assertEqual( aes.y.array, [-3.0, 0.0, 3.0] );
+}
