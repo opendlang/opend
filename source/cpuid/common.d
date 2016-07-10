3fdb4b0e27e0fabda3d1a4ae200fac2cc4053e43
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2016-07-10 16:05:35 +0200

    update common

--- source/cpuid/common.d
+++ source/cpuid/common.d
@@ -3,26 +3,32 @@ Common data types and functions for all architectures.
 +/
 module cpuid.common;
 
-///
+/// Cache Information
 struct  Cache
 {
-    ///
-    uint size; /// KB
-    ///
-    uint associative;
-    ///
-    uint line;
+    /// Cache size in KBs
+    uint size;
+    /// Ways of associativity. Equals `associative.max` if cache is fully associative.
+    ushort associative;
+    /// Cache line in KBs
+    ushort line;
+    /// Logical processors per thread
+    ubyte threads = 1;
+    /// Physical processors per thread
+    ubyte cores = 1;
+    /// `true` if cache is inclusive of lower cache levels.
+    bool inclusive = true;
 }
 
-///
+/// Translation Lookaside Buffer Information
 struct Tlb
 {
-    ///
-    uint page; /// KB
-    ///
-    uint associative;
-    ///
+    /// Page size in KBs
+    uint page;
+    /// Amount of pages TLB
     uint entries;
+    /// Ways of associativity. Equals `associative.max` if TLB is fully associative.
+    ushort associative;
 
     /// Computes size in KBs
     uint size() const @property @safe pure nothrow @nogc
