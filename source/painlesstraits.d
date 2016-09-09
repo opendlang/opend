8c1098d59adb61b6e8febfa23ea42a6e0c04ac74
WebFreak001 <janju007@web.de>
2016-09-09 18:15:59 +0200

    Fixed compilation on old compilers

--- source/painlesstraits.d
+++ source/painlesstraits.d
@@ -208,16 +208,42 @@ private template publicMemberFilter(T, alias filter)
     }
 }
 
+// from std.meta to support older compilers
+private template AliasSeq(TList...)
+{
+    alias AliasSeq = TList;
+}
+
+private template Filter(alias pred, TList...)
+{
+    static if (TList.length == 0)
+    {
+        alias Filter = AliasSeq!();
+    }
+    else static if (TList.length == 1)
+    {
+        static if (pred!(TList[0]))
+            alias Filter = AliasSeq!(TList[0]);
+        else
+            alias Filter = AliasSeq!();
+    }
+    else
+    {
+        alias Filter =
+            AliasSeq!(
+                Filter!(pred, TList[ 0  .. $/2]),
+                Filter!(pred, TList[$/2 ..  $ ]));
+    }
+}
+
 template allPublicFieldsOrProperties(T)
 {
-    import std.meta : Filter;
     enum allPublicFieldsOrProperties = Filter!(publicMemberFilter!(T, isFieldOrProperty).Filter, __traits(allMembers, T));
     static assert(allPublicFieldsOrProperties.length > 0, "No properties for type " ~ T.stringof);
 }
 
 template allPublicFields(T)
 {
-    import std.meta : Filter;
     enum allPublicFields = Filter!(publicMemberFilter!(T, isField).Filter, __traits(allMembers, T));
     static assert(allPublicFields.length > 0, "No properties for type " ~ T.stringof);
 }
