3dc4066d77ee1ae330890ea845a33edca32be5e3
BlackEdder <edder@tkwsping.nl>
2016-10-07 07:25:25 +0100

    Added scale function

--- source/ggplotd/scale.d
+++ source/ggplotd/scale.d
@@ -3,6 +3,11 @@ module ggplotd.scale;
 import cairo = cairo.cairo;
 import ggplotd.bounds : Bounds, width, height;
 
+version (unittest)
+{
+    import dunit.toolkit;
+}
+
 alias ScaleType = 
     cairo.Context delegate(cairo.Context context, in Bounds bounds,
     double width, double height);
@@ -17,4 +22,40 @@ ScaleType scale()
         context.translate(-bounds.min_x, -bounds.min_y);
         return context;
     };
+}
+
+struct ScaleFunction(string type = "")
+{
+    string field = type;
+
+    double delegate(double) scale;
+}
+
+unittest
+{
+    auto sf = ScaleFunction!"bla"();
+    assertEqual(sf.field, "bla");
+}
+
+auto scale(string field = "")(string type)
+{
+    ScaleFunction!field sf;
+    import std.math : log;
+    if (type == "log10") {
+        sf.scale = (v) { return log(v)/log(10.0); };
+    } else if (type == "log") {
+        sf.scale = (v) { return log(v); };
+    } else if (type == "polar") {
+        assert(0, "Polar not implemented yet");
+    } else {
+        // dummy
+        sf.scale = (v) { return v; };
+    }
+    return sf;
+}
+
+unittest
+{
+    auto sf = scale!"bla"("log10");
+    assertEqual(sf.scale(10.0), 1);
 }
