fa4a8559b8418430b533caa633e1ec0d2b3c3f89
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2016-11-24 11:20:40 +0200

    add real uniform random utils

--- source/random/package.d
+++ source/random/package.d
@@ -79,7 +79,7 @@ private template Iota(size_t i, size_t j)
 Params:
     gen = saturated random number generator
 Returns:
-    Uniformly distributed boolean.
+    Uniformly distributed enumeration.
 +/
 T rand(T, G)(ref G gen)
     if (isSaturatedRandomEngine!G && is(T == enum))
@@ -126,9 +126,106 @@ unittest
     auto e = gen.rand!A;
 }
 
-T rand(T, G)(ref G gen, sizediff_t maxExp = 0)
+/++
+Params:
+    gen = saturated random number generator
+    boundExp = bound exponent (optional).
+Returns:
+    Uniformly distributed real for interval `(2^^(-boundExp) , 2^^boundExp)`.
+
+Note: `fabs` can be used to get a value from positive interval `[0, 2^^boundExp)`.
++/
+T rand(T, G)(ref G gen, sizediff_t boundExp = 0)
     if (isSaturatedRandomEngine!G && isFloatingPoint!T)
 {
+    assert(boundExp <= T.max_exp);
+    assert(boundExp >= T.min_exp - 1);
+    static if (is(T == float))
+    {
+        auto d = gen.rand!uint;
+        enum uint EXPMASK = 0x7F80_0000;
+        boundExp -= T.min_exp - 1;
+        uint exp = EXPMASK & d;
+        exp = cast(uint) (boundExp - (exp ? bsf(exp) - (T.mant_dig - 1) : gen.randGeometric));
+        if(cast(int)exp < 0)
+            exp = 0;
+        d = (exp << (T.mant_dig - 1)) ^ (d & ~EXPMASK);
+        return *cast(T*)&d;
+    }
+    else
+    static if (is(T == double))
+    {
+        auto d = gen.rand!ulong;
+        enum ulong EXPMASK = 0x7FF0_0000_0000_0000;
+        boundExp -= T.min_exp - 1;
+        ulong exp = EXPMASK & d;
+        exp = cast(ulong) (boundExp - (exp ? bsf(exp) - (T.mant_dig - 1) : gen.randGeometric));
+        if(cast(int)exp < 0)
+            exp = 0;
+        d = (exp << (T.mant_dig - 1)) ^ (d & ~EXPMASK);
+        return *cast(T*)&d;
+    }
+    else
+    static if (T.mant_dig == 64)
+    {
+        auto d = gen.rand!uint;
+        auto m = gen.rand!ulong;
+        enum uint EXPMASK = 0x7FFF;
+        boundExp -= T.min_exp - 1;
+        uint exp = EXPMASK & d;
+        exp = cast(uint) (boundExp - (exp ? bsf(exp) : gen.randGeometric));
+        if(cast(int)exp < 0)
+            exp = 0;
+        if (exp)
+            m |= 1UL << 63;
+        else
+            m &= long.max;
+        d = exp ^ (d & ~EXPMASK);
+        static union U
+        {
+            T r;
+            struct
+            {
+                version(LittleEndian)
+                {
+                    ulong m;
+                    ushort e;
+                }
+                else
+                {
+                    ushort e;
+                    align(2)
+                    ulong m;
+                }
+            }
+        }
+        U ret = void;
+        ret.e = cast(ushort)d;
+        ret.m = m;
+        return ret.r;
+    }
+    /// TODO: quadruple
+    else static assert(0);
+}
+
+///
+unittest
+{
+    import std.math: fabs;
+    import random.engine.xorshift;
+    auto gen = Xorshift(1);
+    
+    auto a = gen.rand!float;
+    assert(-1 < a && a < +1);
+    
+    auto b = gen.rand!double(4);
+    assert(-16 < b && b < +16);
+    
+    auto c = gen.rand!double(-2);
+    assert(-0.25 < c && c < +0.25);
+    
+    auto d = gen.rand!double.fabs;
+    assert(0 <= d && d < 1);
 }
 
 /++
--- source/random/variable.d
+++ source/random/variable.d
@@ -8,6 +8,11 @@ module random.variable;
 import random;
 import std.traits;
 
+version(LDC)
+    import ldc.intrinsics: fabs = llvm_fabs;
+else
+    import std.math: fabs;
+
 /// User Defined Attribute definition for Random Variable.
 enum RandomVariable;
 
@@ -29,7 +34,7 @@ template isRandomVariable(T)
 
 /++
 Discrete Uniform Random Variable.
-Returns: `X ~ U(a, b)`
+Returns: `X ~ U[a, b]`
 +/
 @RandomVariable struct UniformVariable(T)
     if (isIntegral!T)
@@ -43,7 +48,7 @@ Returns: `X ~ U(a, b)`
     +/
     this(T a, T b)
     {
-        assert(a <= b, "constraint: a < b");
+        assert(a <= b, "constraint: a <= b");
         _length = b - a + 1;
         _offset = a;
     }
@@ -66,8 +71,72 @@ unittest
 {
     import random.engine.xorshift;
     auto gen = Xorshift(1);
-    auto rv = UniformVariable!int(-10, 10); // [-10, 11)
+    auto rv = UniformVariable!int(-10, 10); // [-10, 10]
     auto x = rv(gen); // random variable
     assert(rv.min == -10);
     assert(rv.max == 10);
+}
+
+/++
+Real Uniform Random Variable.
+Returns: `X ~ U[a, b)`
++/
+@RandomVariable struct UniformVariable(T)
+    if (isFloatingPoint!T)
+{
+    import std.math : nextDown, isFinite;
+    private T _a;
+    private T _b;
+
+    /++
+    Constraints: `a < b`, `a` and `b` are finite numbers.
+    +/
+    this(T a, T b)
+    {
+        assert(a < b, "constraint: a < b");
+        assert(a.isFinite);
+        assert(b.isFinite);
+        _a = a;
+        _b = b;
+    }
+
+    ///
+    T opCall(G)(ref G gen)
+        if (isSaturatedRandomEngine!G)
+    {
+        auto ret =  gen.rand!T.fabs * (_b - _a) + _a;
+        if(ret < _b)
+            return ret;
+        return max;
+    }
+
+    ///
+    T min() @property { return _a; }
+    ///
+    T max() @property { return _b.nextDown; }
+}
+
+///
+unittest
+{
+    import std.math : nextDown;
+    import random.engine.xorshift;
+    auto gen = Xorshift(1);
+    auto rv = UniformVariable!double(-8, 10); // [-8, 10)
+    auto x = rv(gen); // random variable
+    assert(rv.min == -8.0);
+    assert(rv.max == 10.0.nextDown);
+}
+
+unittest
+{
+    import std.math : nextDown;
+    import random.engine.xorshift;
+    auto gen = Xorshift(1);
+    auto rv = UniformVariable!double(-8, 10); // [-8, 10)
+    foreach(_; 0..1000)
+    {
+        auto x = rv(gen);
+        assert(rv.min <= x && x <= rv.max);
+    }
 }
