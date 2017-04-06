18a3388e9f7b0e5807bfc75fe4c7bf842d71587c
Atila Neves <atila.neves@gmail.com>
2017-04-07 00:34:46 +0200

    UniqueArray length manipulations

--- source/automem/package.d
+++ source/automem/package.d
@@ -427,13 +427,7 @@ struct UniqueArray(Type, Allocator) if(isArray!Type && isAllocator!Allocator) {
     @disable this(this);
 
     ~this() {
-        import std.experimental.allocator: dispose;
-        import std.traits: isPointer;
-
-        static if(isPointer!Allocator)
-            assert((_objects.length == 0 && _objects.ptr is null) || _allocator !is null);
-
-        if(_objects.ptr !is null) _allocator.dispose(_objects);
+        deleteObjects;
     }
 
     /**
@@ -460,25 +454,68 @@ struct UniqueArray(Type, Allocator) if(isArray!Type && isAllocator!Allocator) {
         moveFrom(other);
     }
 
-    ref inout(Element) opIndex(long i) inout {
+    ref inout(Element) opIndex(long i) inout nothrow {
         return _objects[i];
     }
 
-    inout(Element)[] opSlice(long i, long j) inout {
+    const(Element)[] opSlice(long i, long j) const nothrow {
         return _objects[i .. j];
     }
 
-    long opDollar() const {
+    const(Element)[] opSlice() const nothrow {
+        return _objects[0 .. length];
+    }
+
+    long opDollar() const nothrow {
         return length;
     }
 
-    long length() const {
+    @property long length() const nothrow {
         return _objects.length;
     }
 
+    @property void length(long i) {
+        import std.experimental.allocator: expandArray, shrinkArray;
+
+        if(i > length)
+            _allocator.expandArray(_objects, i - length);
+        else
+            _allocator.shrinkArray(_objects, length - i);
+    }
+
+    /**
+       Dereference. const  since this otherwise could be used to try
+       and append to the array, which would not be nice
+     */
+    ref const(Type) opUnary(string s)() const if(s == "*") {
+        return _objects;
+    }
+
+    void opOpAssign(string op)(Element other) if(op == "~") {
+        import std.experimental.allocator: expandArray;
+
+        _allocator.expandArray(_objects, 1);
+        _objects[$ - 1] = other;
+    }
+
+    void opOpAssign(string op)(Type other) if(op == "~") {
+        import std.experimental.allocator: expandArray;
+        const originalLength = length;
+        _allocator.expandArray(_objects, other.length);
+        _objects[originalLength .. $] = other[];
+    }
+
+    void opOpAssign(string op)(UniqueArray other) if(op == "~") {
+        import std.experimental.allocator: expandArray;
+        const originalLength = length;
+        _allocator.expandArray(_objects, other.length);
+        _objects[originalLength .. $] = other[];
+    }
+
+
 private:
 
-    Element[] _objects;
+    Type _objects;
 
     static if(isSingleton)
         alias _allocator = Allocator.instance;
@@ -489,6 +526,21 @@ private:
     void makeObjects(size_t size) {
         import std.experimental.allocator: makeArray;
         _objects = _allocator.makeArray!Element(size);
+    }
+
+    void deleteObjects() {
+        deleteObjects(_objects);
+    }
+
+
+    void deleteObjects(Element[] objects) {
+        import std.experimental.allocator: dispose;
+        import std.traits: isPointer;
+
+        static if(isPointer!Allocator)
+            assert((objects.length == 0 && objects.ptr is null) || _allocator !is null);
+
+        if(objects.ptr !is null) _allocator.dispose(objects);
     }
 
     void moveFrom(T)(ref UniqueArray!(T, Allocator) other) if(is(T: Type)) {
@@ -537,17 +589,58 @@ version(unittest) {
             Struct.numStructs += 1; // this ends up at -2 for some reason
         }
 
-        Struct.numStructs.shouldEqual(0);
+        ptr.length.shouldEqual(3);
 
         ptr[2].twice.shouldEqual(0);
         ptr[2] = Struct(5);
         ptr[2].twice.shouldEqual(10);
 
-        ptr.length.shouldEqual(3);
         ptr[1..$].shouldEqual([Struct(), Struct(5)]);
 
         typeof(ptr) ptr2;
         move(ptr, ptr2);
+
+        ptr.length.shouldEqual(0);
+        (cast(bool)ptr).shouldBeFalse;
+        ptr2.length.shouldEqual(3);
+        (cast(bool)ptr2).shouldBeTrue;
+
+        // not copyable
+        static assert(!__traits(compiles, ptr2 = ptr1));
+
+        auto ptr3 = ptr2.unique;
+        ptr3.length.shouldEqual(3);
+        ptr3[].shouldEqual([Struct(), Struct(), Struct(5)]);
+        (*ptr3).shouldEqual([Struct(), Struct(), Struct(5)]);
+
+        ptr3 ~= Struct(10);
+        ptr3[].shouldEqual([Struct(), Struct(), Struct(5), Struct(10)]);
+
+        ptr3 ~= [Struct(11), Struct(12)];
+        ptr3[].shouldEqual([Struct(), Struct(), Struct(5), Struct(10), Struct(11), Struct(12)]);
+
+        ptr3.length = 3;
+        ptr3[].shouldEqual([Struct(), Struct(), Struct(5)]);
+
+        ptr3.length = 4;
+        ptr3[].shouldEqual([Struct(), Struct(), Struct(5), Struct()]);
+
+        ptr3.length = 1;
+
+        static if(isSingleton)
+            ptr3 ~= UniqueArray!(Struct[], Allocator)(1);
+        else
+            ptr3 ~= UniqueArray!(Struct[], Allocator)(&allocator, 1);
+
+        ptr3[].shouldEqual([Struct(), Struct()]);
+
+        static if(isSingleton)
+            auto ptr4 = UniqueArray!(Struct[], Allocator)(1);
+        else
+            auto ptr4 = UniqueArray!(Struct[], Allocator)(&allocator, 1);
+
+        ptr3 ~= ptr4.unique;
+        ptr3[].shouldEqual([Struct(), Struct(), Struct()]);
     }
 }
 
