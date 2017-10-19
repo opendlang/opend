faac99e182e426252b10a310e85c0b8120340098
n8sh <natesash@gmail.com>
2017-10-19 10:26:47 -0400

    Allow seeding MersenneTwisterEngine with arrays/ranges (#29)
    * Allow seeding MersenneTwisterEngine with arrays/ranges
    
    Behavior is like the reference implementation's init_by_array,
    not like Phobos std.random.MersenneTwisterEngine's seed(Range).
    
    * Seed MersenneTwisterEngine from any ForwardRange
    Add handling for zero-length arrays
    
    * Rework as requested by @9il
    
    https://github.com/libmir/mir-random/pull/29#issuecomment-337458572
    
    * Fix build failure on LDC
    
    Old version built with DMD 2.074.1 and 2.076.1 but not LDC 1.3.0-beta1 or 1.4.0
    
    * Loop optimizations requested by @9il
    
    https://github.com/libmir/mir-random/pull/29#issuecomment-337805484
    https://github.com/libmir/mir-random/pull/29#discussion_r145604320
    
    * Remove support for seeding from arbitrary InputRange
    
    https://github.com/libmir/mir-random/pull/29#issuecomment-337900806

--- source/mir/random/engine/mersenne_twister.d
+++ source/mir/random/engine/mersenne_twister.d
@@ -8,6 +8,7 @@ Authors: $(HTTP erdani.org, Andrei Alexandrescu) Ilya Yaroshenko (rework)
 module mir.random.engine.mersenne_twister;
 
 import std.traits;
+import mir.ndslice.slice : Slice, SliceKind, Contiguous;
 
 /++
 The $(LUCKY Mersenne Twister) generator.
@@ -87,6 +88,134 @@ struct MersenneTwisterEngine(UIntType, size_t w, size_t n, size_t m, size_t r,
         }
         index = n-1;
         opCall();
+    }
+
+    /++
+    Constructs a MersenneTwisterEngine object.
+
+    Note that `MersenneTwisterEngine([123])` will not result in
+    the same initial state as `MersenneTwisterEngine(123)`.
+    +/
+    this()(scope Slice!(Contiguous, [1], const(UIntType)*) slice) @safe pure nothrow @nogc
+    {
+        static if (is(UIntType == uint))
+        {
+            enum UIntType f2 = 1664525u;
+            enum UIntType f3 = 1566083941u;
+        }
+        else static if (is(UIntType == ulong))
+        {
+            enum UIntType f2 = 3935559000370003845uL;
+            enum UIntType f3 = 2862933555777941757uL;
+        }
+        else
+            static assert(0, "init by slice only supported if UIntType is uint or ulong!");
+
+        data[$-1] = cast(UIntType) (19650218u & max);
+        foreach_reverse (size_t i, ref e; data[0 .. $-1])
+        {
+            e = f * (data[i + 1] ^ (data[i + 1] >> (w - 2))) + cast(UIntType)(n - (i + 1));
+            static if (max != UIntType.max)
+                e &= max;
+        }
+        index = n-1;
+        if (slice.length == 0)
+        {
+            opCall();
+            return;
+        }
+
+        size_t final_mix_index = void;
+
+        if (slice.length >= n)
+        {
+            size_t j = 0;
+            //Handle all but tail.
+            while (slice.length - j >= n - 1)
+            {
+                foreach_reverse (i, ref e; data[0 .. $-1])
+                {
+                    e = (e ^ ((data[i+1] ^ (data[i+1] >> (w - 2))) * f2))
+                        + slice[j] + cast(UIntType) j;
+                    static if (max != UIntType.max)
+                        e &= max;
+                    ++j;
+                }
+                data[$ - 1] = data[0];
+            }
+            //Handle tail.
+            size_t i = n - 2;
+            while (j < slice.length)
+            {
+                data[i] = (data[i] ^ ((data[i+1] ^ (data[i+1] >> (w - 2))) * f2))
+                    + slice[j] + cast(UIntType) j;
+                static if (max != UIntType.max)
+                    data[i] &= max;
+                ++j;
+                --i;
+            }
+            //Set the index for use by the next pass.
+            final_mix_index = i;
+        }
+        else
+        {
+            size_t i = n - 2;
+            //Handle all but tail.
+            while (i >= slice.length)
+            {
+                foreach (j; 0 .. slice.length)
+                {
+                    data[i] = (data[i] ^ ((data[i+1] ^ (data[i+1] >> (w - 2))) * f2))
+                        + slice[j] + cast(UIntType) j;
+                    static if (max != UIntType.max)
+                        data[i] &= max;
+                    --i;
+                }
+            }
+            //Handle tail.
+            size_t j = 0;
+            while (i != cast(size_t) -1)
+            {
+                data[i] = (data[i] ^ ((data[i+1] ^ (data[i+1] >> (w - 2))) * f2))
+                    + slice[j] + cast(UIntType) j;
+                static if (max != UIntType.max)
+                    data[i] &= max;
+                ++j;
+                --i;
+            }
+            data[$ - 1] = data[0];
+            i = n - 2;
+            data[i] = (data[i] ^ ((data[i+1] ^ (data[i+1] >> (w - 2))) * f2))
+                + slice[j] + cast(UIntType) j;
+            static if (max != UIntType.max)
+                data[i] &= max;
+            //Set the index for use by the next pass.
+            final_mix_index = n - 2;
+        }
+
+        foreach_reverse (i, ref e; data[0 .. final_mix_index])
+        {
+            e = (e ^ ((data[i+1] ^ (data[i+1] >> (w - 2))) * f3))
+                - cast(UIntType)(n - (i + 1));
+            static if (max != UIntType.max)
+                e &= max;
+        }
+        foreach_reverse (i, ref e; data[final_mix_index .. n-1])
+        {
+            e = (e ^ ((data[i+1] ^ (data[i+1] >> (w - 2))) * f3))
+                - cast(UIntType)(n - (i + 1));
+            static if (max != UIntType.max)
+                e &= max;
+        }
+        data[$-1] = (cast(UIntType)1) << ((UIntType.sizeof * 8) - 1); /* MSB is 1; assuring non-zero initial array */
+        opCall();
+    }
+
+    /// ditto
+    this()(scope const(UIntType)[] array) @safe pure nothrow @nogc
+    {
+        import mir.ndslice.slice: sliced;
+        this(array.sliced);
     }
 
     /++
@@ -226,4 +355,24 @@ version(mir_random_test) unittest
             a();
         assert(val[i] == a());
     }
+}
+
+@safe nothrow @nogc version(mir_random_test) unittest
+{
+    //Verify that seeding with an array gives the same result as the reference
+    //implementation.
+
+    //32-bit: www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/MT2002/CODES/mt19937ar.tgz
+    immutable uint[4] seed32 = [0x123u, 0x234u, 0x345u, 0x456u];
+    auto gen32 = Mt19937(seed32);
+    foreach(_; 0..999)
+        gen32();
+    assert(3460025646u == gen32());
+
+    //64-bit: www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/mt19937-64.tgz
+    immutable ulong[4] seed64 = [0x12345uL, 0x23456uL, 0x34567uL, 0x45678uL];
+    auto gen64 = Mt19937_64(seed64);
+    foreach(_; 0..999)
+        gen64();
+    assert(994412663058993407uL == gen64());
 }
