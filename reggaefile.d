2d3481a70a62bd07021f97890cbb8806f46da64c
Atila Neves <atila.neves@gmail.com>
2017-10-30 17:23:54 +0100

    Update reggaefile

--- reggaefile.d
+++ reggaefile.d
@@ -1,9 +1,8 @@
 import reggae;
 import std.typecons;
 
-alias lib = dubDefaultTarget!(Flags("-g -debug"));
+alias lib = dubDefaultTarget!(CompilerFlags("-g -debug"));
 alias ut = dubTestTarget!();
-alias utl = dubConfigurationTarget!(ExeName("utl"),
-                                    Configuration("ut"),
-                                    Flags("-unittest -version=unitThreadedLight"));
+alias utl = dubConfigurationTarget!(Configuration("ut"),
+                                    CompilerFlags("-unittest -version=unitThreadedLight"));
 mixin build!(lib, ut, utl);
