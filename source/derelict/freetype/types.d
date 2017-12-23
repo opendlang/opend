ddffb7f879c00fd3c2e64e6f50a6f021bcafbdea
Michael Parker <aldacron@gmail.com>
2017-12-23 18:45:22 +0900

    Fix wrong enum values

--- source/derelict/freetype/types.d
+++ source/derelict/freetype/types.d
@@ -345,8 +345,8 @@ struct FT_GlyphSlotRec {
 enum : uint {
     FT_OPEN_MEMORY           = 0x1,
     FT_OPEN_STREAM           = 0x2,
-    FT_OPEN_DRIVER           = 0x4,
-    FT_OPEN_PATHNAME         = 0x8,
+    FT_OPEN_PATHNAME         = 0x4,
+    FT_OPEN_DRIVER           = 0x8,
     FT_OPEN_PARAMS           = 0x10,
 }
 
