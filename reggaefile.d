f25246a5da18e99ea95ef716ab230739c34735ff
Atila Neves <atila.neves@gmail.com>
2018-01-05 20:22:18 +0100

    Updated reggaefile.d

--- reggaefile.d
+++ reggaefile.d
@@ -1,8 +1,16 @@
 import reggae;
 import std.typecons;
 
-alias lib = dubDefaultTarget!(CompilerFlags("-g -debug"));
-alias ut = dubTestTarget!();
+enum debugFlags = "-w -g -debug";
+static if (__VERSION__ >= 2077)
+    enum allTogether = Yes.allTogether;
+else
+    enum allTogether = Yes.allTogether;
+
+alias lib = dubDefaultTarget!(CompilerFlags(debugFlags));
+alias ut = dubTestTarget!(CompilerFlags(debugFlags),
+                          LinkerFlags(),
+                          allTogether);
 alias utl = dubConfigurationTarget!(Configuration("ut"),
-                                    CompilerFlags("-unittest -version=unitThreadedLight"));
+                                    CompilerFlags("-unittest -version=unitThreadedLight " ~ debugFlags));
 mixin build!(lib, ut, utl);
