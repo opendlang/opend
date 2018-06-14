3032abd52e506fef38200d8a7e9b5f75ab5d6468
Atila Neves <atila.neves@gmail.com>
2018-06-13 21:58:14 -0300

    Moved unique

--- tests/ut_main.d
+++ tests/ut_main.d
@@ -3,10 +3,10 @@ import unit_threaded;
 int main(string[] args) {
     return args.runTests!(
         "automem",
-        "automem.test_utils",
-        "automem.traits",
         "automem.unique",
         "automem.unique_array",
+        "automem.test_utils",
+        "automem.traits",
         "automem.utils",
         "ut.allocator",
         "ut.ref_counted",
