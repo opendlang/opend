21f9603a66733d4bdcbd562dcd20760caa65a225
Atila Neves <atila.neves@gmail.com>
2018-06-13 22:03:44 -0300

    Made utils.d tests @safe @nogc pure

--- tests/ut_main.d
+++ tests/ut_main.d
@@ -4,7 +4,6 @@ int main(string[] args) {
     return args.runTests!(
         "automem",
         "automem.unique",
-        "automem.unique_array",
         "automem.test_utils",
         "automem.traits",
         "automem.utils",
