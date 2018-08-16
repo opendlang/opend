f4227c5cf4ccd3976757b360cdf85a1a090c9a18
Atila Neves <atila.neves@gmail.com>
2018-08-16 18:11:37 +0200

    Failing test for copying an array

--- tests/ut/array.d
+++ tests/ut/array.d
@@ -6,6 +6,7 @@ import automem.array;
 mixin TestUtils;
 
 
+
 @("array.int")
 @safe unittest {
     array(1, 2, 3, 4, 5).should == [1, 2, 3, 4, 5];
@@ -16,4 +17,15 @@ mixin TestUtils;
 @safe unittest {
     array(33.3).should == [33.3];
     array(22.2, 77.7).should == [22.2, 77.7];
+}
+
+@ShouldFail
+@("copying")
+@safe unittest {
+    auto arr1 = array(1, 2, 3);
+    auto arr2 = arr1;
+    arr1[1] = 7;
+
+    arr1.should == [1, 7, 3];
+    arr2.should == [1, 2, 3];
 }
