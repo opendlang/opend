abbb0981cf17e8ab0688ac5c1787e0e09156ca23
Guillaume Piolat <contact@auburnsounds.com>
2018-12-12 16:23:22 +0100

    Fix _mm_mul_epu32 for older LDC

--- source/inteli/emmintrin.d
+++ source/inteli/emmintrin.d
@@ -735,7 +735,17 @@ __m128i _mm_mul_epu32(__m128i a, __m128i b) pure @safe
     __m128i zero = _mm_setzero_si128();
     long2 la = cast(long2) shufflevector!(int4, 0, 4, 2, 6)(a, zero);
     long2 lb = cast(long2) shufflevector!(int4, 0, 4, 2, 6)(b, zero);
-    return cast(__m128i)(la * lb);
+    static if (__VERSION__ >= 2076)
+    {
+        return cast(__m128i)(la * lb);
+    }
+    else
+    {
+        // long2 mul not supported before 
+        la[0] *= lb[0];
+        la[1] *= lb[1];
+        return cast(__m128i)(la);
+    }
 }
 unittest
 {
