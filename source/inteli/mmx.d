ce887eda01d03be2cc945070aa79729a3565246e
Guillaume Piolat <contact@auburnsounds.com>
2019-01-20 20:03:02 +0100

    Implements: _mm_adds_pu16 _mm_adds_pu8

--- source/inteli/mmx.d
+++ source/inteli/mmx.d
@@ -51,7 +51,8 @@ unittest
     assert(R.array == correct);
 }
 
-// PERF: verify codegen for PADDSW
+/// Add packed 16-bit integers in `a` and `b` using signed saturation.
+// PERF: PADDSW not generated
 __m64 _mm_adds_pi16(__m64 a, __m64 b) pure @trusted
 {
     short[4] res;
@@ -69,7 +70,8 @@ unittest
     assert(res.array == correctResult);
 }
 
-// PERF: verify codegen for PADDSB
+/// Add packed 8-bit integers in `a` and `b` using signed saturation.
+// PERF: PADDSB not generated
 __m64 _mm_adds_pi8(__m64 a, __m64 b) pure @trusted
 {
     byte[8] res;
@@ -87,12 +89,43 @@ unittest
     assert(res.array == correctResult);
 }
 
- /+
-paddusw
-__m64 _mm_adds_pu16 (__m64 a, __m64 b) TODO
-paddusb
-__m64 _mm_adds_pu8 (__m64 a, __m64 b) TODO
-+/
+/// Add packed 16-bit integers in `a` and `b` using unsigned saturation.
+// PERF: PADDUSW not generated
+__m64 _mm_adds_pu16(__m64 a, __m64 b) pure @trusted
+{
+    ushort[4] res;
+    short4 sa = cast(short4)a;
+    short4 sb = cast(short4)b;
+    foreach(i; 0..4)
+        res[i] = saturateSignedIntToUnsignedShort(cast(ushort)(sa.array[i]) + cast(ushort)(sb.array[i]));
+    return *cast(__m64*)(res.ptr);
+}
+unittest
+{
+    short4 res = cast(short4) _mm_adds_pu16(_mm_set_pi16(3, 2, cast(short)65535, 0),
+                                            _mm_set_pi16(3, 2, 1, 0));
+    static immutable short[4] correctResult = [0, cast(short)65535, 4, 6];
+    assert(res.array == correctResult);
+}
+
+/// Add packed 8-bit integers in `a` and `b` using unsigned saturation.
+// PERF: PADDUSB not generated
+__m64 _mm_adds_pu8(__m64 a, __m64 b) pure @trusted
+{
+    byte[8] res;
+    byte8 sa = cast(byte8)a;
+    byte8 sb = cast(byte8)b;
+    foreach(i; 0..8)
+        res[i] = saturateSignedWordToUnsignedByte(cast(ubyte)(sa.array[i]) + cast(ubyte)(sb.array[i]));
+    return *cast(__m64*)(res.ptr);
+}
+unittest
+{
+    byte8 res = cast(byte8) _mm_adds_pu8(_mm_set_pi8(7, 6, 5, 4, 3, 2, cast(byte)255, 0),
+                                         _mm_set_pi8(7, 6, 5, 4, 3, 2, 1, 0));
+    static immutable byte[8] correctResult = [0, cast(byte)255, 4, 6, 8, 10, 12, 14];
+    assert(res.array == correctResult);
+}
 
 /// Compute the bitwise AND of 64 bits (representing integer data) in `a` and `b`.
 __m64 _mm_and_si64 (__m64 a, __m64 b) pure @safe
