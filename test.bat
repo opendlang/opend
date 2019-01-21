a72a86a2006cc7bd49bc98c77dc2d22daf7065fa
Guillaume Piolat <contact@auburnsounds.com>
2019-01-21 01:22:24 +0100

    More extensive test suite. now testing in CI with optimizations + assertions ON.

---
+++ test.bat
@@ -0,0 +1,8 @@
+dub test --compiler dmd -a x86
+dub test --compiler dmd -a x86_64
+dub test --compiler ldc2 -a x86
+dub test --compiler ldc2 -a x86_64
+dub test --compiler dmd -a x86 -b unittest-release
+dub test --compiler dmd -a x86_64 -b unittest-release 
+dub test --compiler ldc2 -b unittest-release 
+dub test --compiler ldc2 -b unittest-release 
