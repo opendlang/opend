1802592c2fb1927e84572452e417bd2136b78ead
Guillaume Piolat <contact@auburnsounds.com>
2019-02-09 02:07:30 +0100

    Started image embeding, works approximately in SVG. the problem is that to be correct we have to parse metadata of image, namely DPI.

---
+++ examples/embed-image/source/app.d
@@ -0,0 +1,28 @@
+import std.stdio;
+import std.file;
+import std.math;
+
+import printed.canvas;
+
+void main(string[] args)
+{
+    auto pdfDoc = new PDFDocument();
+    auto svgDoc = new SVGDocument();
+    auto htmlDoc = new HTMLDocument();
+
+    Image png = new Image("smiley.png");
+    Image jpeg = new Image("smiley.jpg");
+
+    foreach(context; [cast(IRenderingContext2D) pdfDoc, 
+                      cast(IRenderingContext2D) svgDoc,
+                      cast(IRenderingContext2D) htmlDoc,])
+    {        
+        context.drawImage(png, 10, 10);        
+        context.drawImage(jpeg, 10, 20);
+    }
+
+    /// Draw the result of each specific renderer.
+    std.file.write("output.pdf", pdfDoc.bytes);
+    std.file.write("output.svg", svgDoc.bytes);
+    std.file.write("output.html", htmlDoc.bytes);
+}
