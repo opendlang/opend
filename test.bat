90bdd5996d0e73d9b6b39ba2ee2424be9e8e36f3
Guillaume Piolat <contact@auburnsounds.com>
2019-08-08 15:10:54 +0200

    Test with SSE3 enabled / not-enabled (with LDC)

--- test.bat
+++ test.bat
@@ -2,7 +2,11 @@ dub test --compiler dmd -a x86
 dub test --compiler dmd -a x86_64
 dub test --compiler ldc2 -a x86
 dub test --compiler ldc2 -a x86_64
+dub test --compiler ldc2 -a x86 -b unittest-inst
+dub test --compiler ldc2 -a x86_64 -b unittest-inst
 dub test --compiler dmd -a x86 -b unittest-release
 dub test --compiler dmd -a x86_64 -b unittest-release 
-dub test --compiler ldc2 -b unittest-release 
-dub test --compiler ldc2 -b unittest-release 
+dub test --compiler ldc2 -a x86 -b unittest-release 
+dub test --compiler ldc2 -a x86_64 -b unittest-release 
+dub test --compiler ldc2 -a x86 -b unittest-release-inst
+dub test --compiler ldc2 -a x86_64 -b unittest-release-inst
