5a788550151d0167c0bfdec3b2dd0d361a2d243a
Guillaume Piolat <contact@auburnsounds.com>
2019-08-09 15:08:36 +0200

    pmmintrin.d (SSE3) ported for GDC, however none of these intrinsics will be fast with GDC. We still don't have a way to check for SSE3 codegen support in GDC, GCC would define __SSE3__ define... GDC doesn't.

--- source/inteli/pmmintrin.d
+++ source/inteli/pmmintrin.d
@@ -23,15 +23,16 @@ nothrow @nogc:
 __m128d _mm_addsub_pd (__m128d a, __m128d b) pure @safe
 {
     // Note: generates addsubpd since LDC 1.3.0 with -O1
-    a[0] = a[0] - b[0];
-    a[1] = a[1] + b[1];
+    // PERF: for GDC, detect SSE3 and use the relevant builtin
+    a.array[0] = a.array[0] - b.array[0];
+    a.array[1] = a.array[1] + b.array[1];
     return a;
 }
 unittest
 {
     auto v1 =_mm_setr_pd(1.0,2.0);
     auto v2 =_mm_setr_pd(1.0,2.0);
-    assert(_mm_addsub_pd(v1,v2)==_mm_setr_pd(0.0,4.0));
+    assert(_mm_addsub_pd(v1,v2).array == _mm_setr_pd(0.0,4.0).array);
 }
 
 /// Alternatively add and subtract packed single-precision (32-bit) 
@@ -39,17 +40,18 @@ unittest
 float4 _mm_addsub_ps (float4 a, float4 b) pure @safe
 {
     // Note: generates addsubps since LDC 1.3.0 with -O1
-    a[0] -= b[0];
-    a[1] += b[1];
-    a[2] -= b[2];
-    a[3] += b[3];    
+    // PERF: for GDC, detect SSE3 and use the relevant builtin
+    a.array[0] -= b.array[0];
+    a.array[1] += b.array[1];
+    a.array[2] -= b.array[2];
+    a.array[3] += b.array[3];    
     return a;
 }
 unittest
 {
     auto v1 =_mm_setr_ps(1.0f, 2.0f, 3.0f, 4.0f);
     auto v2 =_mm_setr_ps(1.0f, 2.0f, 3.0f, 4.0f);
-    assert( _mm_addsub_ps(v1,v2) == _mm_setr_ps(0.0f, 4.0f, 0.0f, 8.0f) );
+    assert( _mm_addsub_ps(v1,v2).array == _mm_setr_ps(0.0f, 4.0f, 0.0f, 8.0f).array );
 }
 
 version(LDC)
@@ -73,13 +75,15 @@ version(LDC)
 }
 else
 {
+    // PERF: for GDC, detect SSE3 and use the relevant builtin
+
     /// Horizontally add adjacent pairs of double-precision (64-bit) 
     /// floating-point elements in `a` and `b`.
     __m128d _mm_hadd_pd (__m128d a, __m128d b) pure @safe
     {
         __m128d res;
-        res[0] = a[1] + a[0];
-        res[1] = b[1] + b[0];
+        res.array[0] = a.array[1] + a.array[0];
+        res.array[1] = b.array[1] + b.array[0];
         return res;
     }
 }
@@ -87,7 +91,7 @@ unittest
 {
     auto A =_mm_setr_pd(1.5, 2.0);
     auto B =_mm_setr_pd(1.0, 2.0);
-    assert( _mm_hadd_pd(A, B) ==_mm_setr_pd(3.5, 3.0) );
+    assert( _mm_hadd_pd(A, B).array ==_mm_setr_pd(3.5, 3.0).array );
 }
 
 version(LDC)
@@ -113,15 +117,17 @@ version(LDC)
 }
 else
 {
+    // PERF: for GDC, detect SSE3 and use the relevant builtin
+
     /// Horizontally add adjacent pairs of single-precision (32-bit) 
     /// floating-point elements in `a` and `b`.
     __m128 _mm_hadd_ps (__m128 a, __m128 b) pure @safe
     {
         __m128 res;
-        res[0] = a[1] + a[0];
-        res[1] = a[3] + a[2];
-        res[2] = b[1] + b[0];
-        res[3] = b[3] + b[2];
+        res.array[0] = a.array[1] + a.array[0];
+        res.array[1] = a.array[3] + a.array[2];
+        res.array[2] = b.array[1] + b.array[0];
+        res.array[3] = b.array[3] + b.array[2];
         return res;
     }
 }
@@ -129,7 +135,7 @@ unittest
 {
     __m128 A =_mm_setr_ps(1.0f, 2.0f, 3.0f, 5.0f);
     __m128 B =_mm_setr_ps(1.5f, 2.0f, 3.5f, 4.0f);
-    assert(_mm_hadd_ps(A, B)==_mm_setr_ps(3.0f, 8.0f, 3.5f, 7.5f));
+    assert( _mm_hadd_ps(A, B).array == _mm_setr_ps(3.0f, 8.0f, 3.5f, 7.5f).array );
 }
 
 version(LDC)
@@ -155,11 +161,13 @@ else
 {
     /// Horizontally subtract adjacent pairs of double-precision (64-bit) 
     /// floating-point elements in `a` and `b`.
+    // PERF: for GDC, detect SSE3 and use the relevant builtin
+
     __m128d _mm_hsub_pd (__m128d a, __m128d b) pure @safe
     {
         __m128d res;
-        res[0] = a[0] - a[1];
-        res[1] = b[0] - b[1];
+        res.array[0] = a.array[0] - a.array[1];
+        res.array[1] = b.array[0] - b.array[1];
         return res;
     }
 }
@@ -167,7 +175,7 @@ unittest
 {
     auto A =_mm_setr_pd(1.5, 2.0);
     auto B =_mm_setr_pd(1.0, 2.0);
-    assert( _mm_hsub_pd(A, B) ==_mm_setr_pd(-0.5, -1.0) );
+    assert( _mm_hsub_pd(A, B).array ==_mm_setr_pd(-0.5, -1.0).array );
 }
 
 version(LDC)
@@ -197,11 +205,12 @@ else
     /// floating-point elements in `a` and `b`.
     __m128 _mm_hsub_ps (__m128 a, __m128 b) pure @safe
     {
+        // PERF: GDC probably doesn't generate the right instruction
         __m128 res;
-        res[0] = a[0] - a[1];
-        res[1] = a[2] - a[3];
-        res[2] = b[0] - b[1];
-        res[3] = b[2] - b[3];
+        res.array[0] = a.array[0] - a.array[1];
+        res.array[1] = a.array[2] - a.array[3];
+        res.array[2] = b.array[0] - b.array[1];
+        res.array[3] = b.array[2] - b.array[3];
         return res;
     }
 }
@@ -209,7 +218,7 @@ unittest
 {
     __m128 A =_mm_setr_ps(1.0f, 2.0f, 3.0f, 5.0f);
     __m128 B =_mm_setr_ps(1.5f, 2.0f, 3.5f, 4.0f);
-    assert(_mm_hsub_ps(A, B)==_mm_setr_ps(-1.0f, -2.0f, -0.5f, -0.5f));
+    assert(_mm_hsub_ps(A, B).array == _mm_setr_ps(-1.0f, -2.0f, -0.5f, -0.5f).array);
 }
 
 /// Load 128-bits of integer data from unaligned memory.
@@ -223,34 +232,36 @@ __m128d _mm_loaddup_pd (const(double)* mem_addr) pure @safe
     // Note: generates movddup since LDC 1.3 with -O1 -mattr=+sse3
     double value = *mem_addr;
     __m128d res;
-    res[0] = value;
-    res[1] = value;
+    res.array[0] = value;
+    res.array[1] = value;
     return res;
 }
 unittest
 {
     double a = 7.5;
-    assert(_mm_loaddup_pd(&a) == _mm_set_pd(7.5, 7.5));
+    assert(_mm_loaddup_pd(&a).array == _mm_set_pd(7.5, 7.5).array);
 }
 
 __m128d _mm_movedup_pd (__m128d a) pure @safe
 {
     // Note: generates movddup since LDC 1.3 with -O1 -mattr=+sse3
-    a[1] = a[0];
+    // PERF: GDC probably doesn't generate it
+    a.array[1] = a.array[0];
     return a;
 }
 unittest
 {
     __m128d A = _mm_setr_pd(7.0, 2.5);
-    assert(_mm_movedup_pd(A) == _mm_set_pd(7.0, 7.0));
+    assert(_mm_movedup_pd(A).array == _mm_set_pd(7.0, 7.0).array);
 }
 
 /// Duplicate odd-indexed single-precision (32-bit) floating-point elements from `a`.
 __m128 _mm_movehdup_ps (__m128 a) pure @safe
 {
     // Generates movshdup since LDC 1.3 with -O1 -mattr=+sse3
-    a[0] = a[1];
-    a[2] = a[3];
+    // PERF: GDC probably doesn't generate it
+    a.array[0] = a.array[1];
+    a.array[2] = a.array[3];
     return a;
 }
 
@@ -258,7 +269,8 @@ __m128 _mm_movehdup_ps (__m128 a) pure @safe
 __m128 _mm_moveldup_ps (__m128 a) pure @safe
 {
     // Generates movsldup since LDC 1.3 with -O1 -mattr=+sse3
-    a[1] = a[0];
-    a[3] = a[2];
+    // PERF: GDC probably doesn't generate it
+    a.array[1] = a.array[0];
+    a.array[3] = a.array[2];
     return a;
 }
