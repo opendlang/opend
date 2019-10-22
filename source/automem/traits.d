0d0b91942a1081a443c4192e3a05caa07d614cb3
jmh530 <jmh530@users.noreply.github.com>
2019-10-22 07:21:29 -0400

    Adds functions to traits.d
    Adds isUnique, UniqueType, isRefCounted, RefCountedType.

--- source/automem/traits.d
+++ source/automem/traits.d
@@ -1,5 +1,6 @@
 module automem.traits;
 
+import std.typecons: Flag;
 
 void checkAllocator(T)() {
     import std.experimental.allocator: make, dispose;
@@ -42,4 +43,142 @@ template isSingleton(Allocator) {
 template isTheAllocator(Allocator) {
     import std.experimental.allocator: theAllocator;
     enum isTheAllocator = is(Allocator == typeof(theAllocator));
+}
+
+/**
+   Determines if a type is Unique with a supported Allocator.
+ */
+template isUnique(T) {
+    import automem.unique: Unique;
+
+	static if (is(T == Unique!(Type, Allocator, supportsGC), 
+                  Type, Allocator, Flag!"supportGC" supportsGC))
+    {
+        static if (isAllocator!Allocator)
+            enum bool isUnique = true;
+        else
+            enum bool isUnique = false;
+    }
+    else
+    {
+        enum bool isUnique = false;
+    }
+}
+
+///
+@("isUnique")
+@safe @nogc pure unittest {
+    import automem.unique: Unique;
+
+    static struct Point {
+        int x;
+        int y;
+    }
+
+	auto u = Unique!Point(2, 3);
+	static assert(isUnique!(typeof(u)));
+
+    auto p = Point(2, 3);
+	static assert(!isUnique!(typeof(p)));
+}
+
+/**
+   The base type of a `Unique` pointer.
+ */
+template UniqueType(T)
+{
+    import automem.unique: Unique;
+
+    static if (isUnique!T) {
+        static if (is(T == Unique!(Type, Allocator, supportsGC), 
+                      Type, Allocator, Flag!"supportGC" supportsGC))
+            alias UniqueType = Type;
+        else
+            alias UniqueType = void;
+    } else {
+        static assert(0, "UniqueType: input type is not a valid Unique type");
+    }
+}
+
+///
+@("Get the base type of a Unique type")
+@safe @nogc pure unittest {
+    import automem.unique: Unique;
+
+    static struct Point {
+        int x;
+        int y;
+    }
+
+	auto u = Unique!Point(2, 3);
+	static assert(is(Point == UniqueType!(typeof(u))));
+}
+
+/**
+   Determines if a type is RefCounted with a supported Allocator.
+ */
+template isRefCounted(T) {
+    import automem.ref_counted: RefCounted;
+
+	static if (is(T == RefCounted!(Type, Allocator, supportsGC), 
+                  Type, Allocator, Flag!"supportGC" supportsGC))
+    {
+        static if (isAllocator!Allocator)
+            enum bool isRefCounted = true;
+        else
+            enum bool isRefCounted = false;
+    }
+    else
+    {
+        enum bool isRefCounted = false;
+    }
+}
+
+///
+@("isRefCounted")
+@safe @nogc pure unittest {
+    import automem.ref_counted: RefCounted;
+    
+    static struct Point {
+        int x;
+        int y;
+    }
+
+	auto s = RefCountedType!Point(2, 3);
+	static assert(isRefCounted!(typeof(s)));
+
+    auto p = Point(2, 3);
+    static assert(!isRefCounted!(typeof(p)));
+}
+
+/**
+   The base type of a `RefCounted` pointer.
+ */
+template RefCountedType(T)
+{
+    import automem.ref_counted: RefCounted;
+
+    static if (isRefCounted!T) {
+        static if (is(T == RefCounted!(Type, Allocator, supportsGC), 
+                      Type, Allocator, Flag!"supportGC" supportsGC))
+            alias RefCountedType = Type;
+        else
+            alias RefCountedType = void;
+    } else {
+    	static assert(0, "RefCountedType: input type is not a valid RefCounted type");
+    }
+}
+
+///
+@("Get the base type of a RefCounted type")
+@safe @nogc pure unittest {
+    import automem.ref_counted: RefCounted;
+
+    static struct Point {
+        int x;
+        int y;
+    }
+
+	auto s = RefCounted!Point(2, 3);
+	static assert(is(Point == RefCountedType!(typeof(s))));
 }
