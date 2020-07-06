6d17152d25e5db499cdec51cfd7982b980be88e1
jmh530 <jmh530@users.noreply.github.com>
2020-07-06 06:19:03 -0400

    set up initial file structure and public imports

---
+++ source/mir/descriptive.d
@@ -0,0 +1,5 @@
+module mir.stat.descriptive;
+
+public import mir.math.stat: gmean, GMeanAccumulator, hmean, mean,
+    MeanAccumulator, median, standardDeviation, variance, VarianceAccumulator,
+    VarianceAlgo;
---
+++ source/mir/transform.d
@@ -0,0 +1,3 @@
+module mir.stat.transform;
+
+public import mir.math.stat: center;
