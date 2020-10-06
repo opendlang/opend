1e40d7b89231e115d22e8c46d56c713090bdfc47
ilya <ilyayaroshenko@gmail.com>
2020-10-06 14:28:51 +0700

    fix links

--- source/mir/stat/package.d
+++ source/mir/stat/package.d
@@ -9,8 +9,8 @@ Copyright: 2020 Mir Stat Authors.
 
 Macros:
 SUBREF = $(REF_ALTTEXT $(TT $2), $2, mir, stat, $1)$(NBSP)
-MATHREF = $(REF_ALTTEXT $(TT $2), $2, mir, math, $1)$(NBSP)
-NDSLICEREF = $(REF_ALTTEXT $(TT $2), $2, mir, ndslice, $1)$(NBSP)
+MATHREF = $(GREF_ALTTEXT mir-algorithm, $(TT $2), $2, mir, math, $1)$(NBSP)
+NDSLICEREF = $(GREF_ALTTEXT mir-algorithm, $(TT $2), $2, mir, ndslice, $1)$(NBSP)
 T2=$(TR $(TDNW $(LREF $1)) $(TD $+))
 T4=$(TR $(TDNW $(LREF $1)) $(TD $2) $(TD $3) $(TD $4))
 
