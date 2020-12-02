1ce0c0bbb331a58f2b839366430fe4d62727ed88
ilya <ilyayaroshenko@gmail.com>
2020-12-02 18:08:23 +0700

    fixup

--- source/mir/internal/meta.d
+++ source/mir/internal/meta.d
@@ -13,17 +13,23 @@ template isMemberType(T, string member)
 
 template AllMembersRec(T)
 {
-    static if (__traits(getAliasThis, T).length)
-    {
-        import std.meta: Filter, AliasSeq;
-        T* aggregate;
-        alias baseMembers = AllMembersRec!(typeof(__traits(getMember, aggregate, __traits(getAliasThis, T))));
-        alias members = Erase!(__traits(getAliasThis, T)[0], __traits(allMembers, T));
-        alias AllMembersRec = NoDuplicates!(AliasSeq!(baseMembers, members));
-    }
+    static if (is(T == class) || is(T == struct) || is(T == union) || is(T == interface))
+        static if (__traits(getAliasThis, T).length)
+        {
+            import std.meta: Filter, AliasSeq;
+            T* aggregate;
+            alias baseMembers = AllMembersRec!(typeof(__traits(getMember, aggregate, __traits(getAliasThis, T))));
+            alias members = Erase!(__traits(getAliasThis, T)[0], __traits(allMembers, T));
+            alias AllMembersRec = NoDuplicates!(AliasSeq!(baseMembers, members));
+        }
+        else
+        {
+            alias AllMembersRec = __traits(allMembers, T);
+        }
     else
     {
-        alias AllMembersRec = __traits(allMembers, T);
+        import std.meta: AliasSeq;
+        alias AllMembersRec = AliasSeq!();
     }
 }
 
