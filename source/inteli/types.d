b8c1ec732ea3130b87a57d92c6111981fed2f90d
Guillaume Piolat <contact@auburnsounds.com>
2020-12-17 01:04:01 +0100

    Previous commit was an incomplete fix.

--- source/inteli/types.d
+++ source/inteli/types.d
@@ -284,7 +284,10 @@ static if (CoreSimdIsEmulated)
         // Note: can't be @safe with this signature
         void storeUnaligned(Vec v, BaseType!Vec* pvec) @trusted
         {
-            static if (is(Vec == __vector))
+            enum bool isVector = ( (Vec.sizeof == 8)  && (!MMXSizedVectorsAreEmulated)
+                                || (Vec.sizeof == 16) && (!SSESizedVectorsAreEmulated) );
+
+            static if (isVector)
             {
                 // PERF: there is probably something faster to do for this compiler (DMD).
                 //       Avoid this on DMD in the future.
