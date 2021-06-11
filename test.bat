bf3740bc5fed6e531a82d309c02cd14a6eb341b3
Guillaume Piolat <contact@auburnsounds.com>
2021-06-11 17:14:04 +0200

    Add --force to test script

--- test.bat
+++ test.bat
@@ -1,12 +1,12 @@
-dub test --compiler dmd -a x86
-dub test --compiler dmd -a x86_64
-dub test --compiler ldc2 -a x86
-dub test --compiler ldc2 -a x86_64
-dub test --compiler ldc2 -a x86 -b unittest-inst
-dub test --compiler ldc2 -a x86_64 -b unittest-inst
-dub test --compiler dmd -a x86 -b unittest-release
-dub test --compiler dmd -a x86_64 -b unittest-release 
-dub test --compiler ldc2 -a x86 -b unittest-release 
-dub test --compiler ldc2 -a x86_64 -b unittest-release 
-dub test --compiler ldc2 -a x86 -b unittest-release-inst
-dub test --compiler ldc2 -a x86_64 -b unittest-release-inst
+dub test --compiler dmd -a x86 -f
+dub test --compiler dmd -a x86_64 -f
+dub test --compiler ldc2 -a x86 -f
+dub test --compiler ldc2 -a x86_64 -f
+dub test --compiler ldc2 -a x86 -b unittest-inst -f
+dub test --compiler ldc2 -a x86_64 -b unittest-inst -f
+dub test --compiler dmd -a x86 -b unittest-release -f
+dub test --compiler dmd -a x86_64 -b unittest-release -f 
+dub test --compiler ldc2 -a x86 -b unittest-release -f
+dub test --compiler ldc2 -a x86_64 -b unittest-release -f
+dub test --compiler ldc2 -a x86 -b unittest-release-inst -f
+dub test --compiler ldc2 -a x86_64 -b unittest-release-inst -f
