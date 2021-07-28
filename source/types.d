9a253f61a21cba99cc9cd6a188496a1e3cffb66f
Guillaume Piolat <contact@auburnsounds.com>
2021-07-28 15:42:57 +0200

    it's a start

---
+++ source/types.d
@@ -0,0 +1,10 @@
+module gamut.types;
+
+// Various public types
+
+enum ImageFormat
+{
+	unknown,
+	png, // Portable Network Graphics (*.PNG)
+	jpeg // Independent JPEG Group (*.JPG, *.JIF, *.JPEG, *.JPE)
+}
