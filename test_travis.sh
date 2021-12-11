7ddb0e689f071cb7b822e2018a3ff2f23ff6b5e4
9il <ilyayaroshenko@gmail.com>
2021-12-11 14:27:01 +0700

    Move bloomberg API out

--- test_travis.sh
+++ test_travis.sh
@@ -2,9 +2,7 @@ echo $ARCH
 if [ "$ARCH" = "AARCH64" ]
 then
   dub test --arch=aarch64 --build=unittest
-  dub build --arch=aarch64 -c bloomberg
 else
   # We only upload coverage data on x86/x86_64
   dub test --arch=$ARCH --build=unittest-cov
-  dub build --arch=$ARCH -c bloomberg
 fi
