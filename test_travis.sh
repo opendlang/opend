b7bee6185fe6d7a522db81862e3995ca0e44315e
ilya <ilyayaroshenko@gmail.com>
2022-02-15 14:07:54 +0700

    rm test_travis.sh

--- test_travis.sh
+++
@@ -1,8 +0,0 @@
-echo $ARCH
-if [ "$ARCH" = "AARCH64" ]
-then
-  dub test --arch=aarch64 --build=unittest
-else
-  # We only upload coverage data on x86/x86_64
-  dub test --arch=$ARCH --build=unittest-cov
-fi
