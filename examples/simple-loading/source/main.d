303e0987ed069f481159d5a63c3602727ae16f48
Guillaume Piolat <contact@auburnsounds.com>
2022-03-11 14:21:22 +0100

    Git fail

--- examples/simple-loading/source/main.d
+++ examples/simple-loading/source/main.d
@@ -4,5 +4,9 @@ import gamut;
 
 void main(string[] args)
 {
-
+    FIBITMAP *bitmap = FreeImage_AllocateT(FIT_RGB16, 512, 512);
+    if (bitmap) {
+        // bitmap successfully created!
+        FreeImage_Unload(bitmap);
+    }
 }
