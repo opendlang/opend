993297ce14245399194f03eeef4dca5ba15e3bfc
Guillaume Piolat <contact@auburnsounds.com>
2022-04-30 11:21:21 +0200

    Test DMD with AVX on

--- test.bat
+++ test.bat
@@ -2,11 +2,15 @@ dub test --compiler dmd -a x86 -f
 dub test --compiler dmd -a x86_64 -f
 dub test --compiler ldc2 -a x86 -f
 dub test --compiler ldc2 -a x86_64 -f
+dub test --compiler dmd -a x86 -b unittest-inst -f
+dub test --compiler dmd -a x86_64 -b unittest-inst -f
 dub test --compiler ldc2 -a x86 -b unittest-inst -f
 dub test --compiler ldc2 -a x86_64 -b unittest-inst -f
 dub test --compiler dmd -a x86 -b unittest-release -f
 dub test --compiler dmd -a x86_64 -b unittest-release -f 
 dub test --compiler ldc2 -a x86 -b unittest-release -f
 dub test --compiler ldc2 -a x86_64 -b unittest-release -f
+dub test --compiler dmd -a x86 -b unittest-release-inst -f
+dub test --compiler dmd -a x86_64 -b unittest-release-inst -f
 dub test --compiler ldc2 -a x86 -b unittest-release-inst -f
 dub test --compiler ldc2 -a x86_64 -b unittest-release-inst -f
