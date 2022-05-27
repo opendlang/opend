6ad0200196e33af5fbb461ddded3eb436bc71831
Guillaume Piolat <contact@auburnsounds.com>
2022-05-27 15:26:51 +0200

    Add timings to example.

--- examples/simple-loading/source/main.d
+++ examples/simple-loading/source/main.d
@@ -2,19 +2,36 @@ import gamut;
 
 static import std.file;
 import std.stdio;
-void main(string[] args) @trusted
+
+
+long getTickMs() nothrow @nogc @safe
+{
+    import core.time;
+    return convClockFreq(MonoTime.currTime.ticks, MonoTime.ticksPerSecond, 1_000);
+}
+
+
+void main(string[] args) @safe
 {
     Image image;
-  /*  image.loadFromFile("material.png");
 
-    writefln("width x height = %s x %s", image.width, image.height);
+    long tBefore = getTickMs();
+    image.loadFromFile("material.png");
+    long tAfter = getTickMs();
+
+    writefln("Loaded material.png from file, in %s ms", tAfter - tBefore);
+    writefln("  => width x height = %s x %s", image.width, image.height);
     assert(image.width == 1252);
-    assert(image.height == 974);*/
+    assert(image.height == 974);
 
-    bool success = image.loadFromMemory( std.file.read("material.png") );
+    void[] data = std.file.read("material.png");
+    tBefore = getTickMs();
+    bool success = image.loadFromMemory( data );
+    tAfter = getTickMs();
     assert(success);
 
-    writefln("width x height = %s x %s", image.width, image.height);
+    writefln("Loaded material.png from memory, in %s ms", tAfter - tBefore);
+    writefln("  => width x height = %s x %s", image.width, image.height);
     assert(image.width == 1252);
     assert(image.height == 974);
 }
