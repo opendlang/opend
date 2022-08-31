72130c4b65af9664cd55c317738ab92133600026
Guillaume Piolat <contact@auburnsounds.com>
2022-08-31 16:22:21 +0200

    Renable back wrongly disabled GDC builtins

--- source/inteli/smmintrin.d
+++ source/inteli/smmintrin.d
@@ -143,13 +143,11 @@ __m128i _mm_blendv_epi8 (__m128i a, __m128i b, __m128i mask) @trusted
 {
     // PERF DMD
     // TODO BUG GDC version
-    /*
     static if (GDC_with_SSE41)
     {
         return cast(__m128i) __builtin_ia32_pblendvb(cast(byte16)a, cast(byte16)b, cast(byte16)mask);
     }
-    else
-    */ static if (LDC_with_SSE41)
+    else static if (LDC_with_SSE41)
     {
         return cast(__m128i) __builtin_ia32_pblendvb(cast(byte16)a, cast(byte16)b, cast(byte16)mask);
     }
