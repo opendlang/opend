8cf7049adb6f7f212afbbf11733b868d38e5fcb1
ilya <ilyayaroshenko@gmail.com>
2022-09-23 13:54:36 +0700

    Rework Csv concept

--- source/mir/csv.d
+++ source/mir/csv.d
@@ -1,21 +1,22 @@
 /++
 DRAFT
-
-Then CSV should have the following options to be converted to Ion:
- - matrix
- - an array of records with inner keys in the first raw
- - a record of arrays with outer keys in the first column
- - a record of records with inner keys in the first raw and outer keys in the first column
-
-These four kinds of conversion kinds allow converting CSV to Ion on the fly.
-
-Also, a simple transposition with full memory allocation will allow other four conversions:
- - transposed matrix
- - an array of records with inner keys in the first column
- - a record of arrays with outer keys in the first raw
- - a record of records with inner keys in the first column and outer keys in the first raw
 +/
 module mir.csv;
+
+///
+enum CsvKind
+{
+    ///
+    matrix,
+    ///
+    transposedMatrix,
+    ///
+    objectOfColumns,
+    ///
+    indexedRaws,
+    ///
+    indexedObjects,
+}
 
 /++
 +/
@@ -24,23 +25,21 @@ struct Csv
     ///
     const(char)[] text;
     ///
-    bool header = false;
-    ///
-    bool columnIndex = false;
+    CsvKind kind;
     ///
     bool stripUnquoted = true; 
     ///
-    char sep = ',';
+    char separator = ',';
     ///
     char comment = '#';
+
 
     void serialize(S)(scope ref S serializer) scope const
     {
         // DRAFT
         // TODO: have to be @nogc
         // TODO: support only matrix for now, have to support all
-        assert(!header, "not implemented");
-        assert(!columnIndex, "not implemented");
+        assert(kind == CsvKind.matrix, "not implemented");
         import std.string;
         import std.algorithm.iteration: splitter;
         import std.algorithm.searching: canFind;
@@ -51,7 +50,7 @@ struct Csv
             if (line.length && line[0] == comment)
                 continue;
             auto state = serializer.listBegin;
-            foreach (value; splitter(line, sep))
+            foreach (value; splitter(line, separator))
             {
                 if (stripUnquoted)
                     value = value.strip;
@@ -87,29 +86,6 @@ struct Csv
     }
 }
 
-/++
-+/
-struct TranposedCsv
-{
-    ///
-    Csv csv;
-
-    ///
-    void serialize(S)(scope ref S serializer) scope const
-    {
-        import mir.algebraic_alias.csv: CsvAlgebraic;
-        import mir.ndslice.slice: Slice;
-        import mir.rc.array: RCI;
-        import mir.ion.conv: serde;
-        import mir.ser: serializeValue;
-
-        alias CsvMatrix = Slice!(RCI!CsvAlgebraic, 2);
-        auto matrix = csv.serde!CsvMatrix;
-        auto scopeMatrix = matrix.lightScope;
-        serializer.serializeValue(scopeMatrix);
-    }
-}
-
 /// Matrix
 unittest
 {
@@ -119,9 +95,6 @@ unittest
     auto text = "1,2\n3,4";
     auto matrix = text.Csv.serde!(Slice!(int*, 2));
     assert(matrix == [[1, 2], [3, 4]]);
-
-    matrix = text.Csv.TranposedCsv.serde!(Slice!(int*, 2));
-    assert(matrix == [[1, 3], [2, 4]]);
 }
 
 /++
