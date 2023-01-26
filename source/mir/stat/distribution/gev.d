1b2cca3219fc4dc48630682f6e2b46ab2c24e93e
John Michael Hall <john.michael.hall@gmail.com>
2023-01-25 23:17:16 -0500

    Add gevLPDF/gevCCDF with some minor gev fixups

--- source/mir/stat/distribution/gev.d
+++ source/mir/stat/distribution/gev.d
@@ -50,7 +50,8 @@ version(mir_stat_test)
 @safe pure nothrow @nogc
 unittest
 {
-    import mir.test;
+    import mir.test: shouldApprox;
+
     gevPDF(-3, 2, 3, -0.5).shouldApprox == 0.02120353011709564;
     gevPDF(-1, 2, 3, +0.5).shouldApprox == 0.04884170370329114;
     gevPDF(-1, 2, 3, 0.0).shouldApprox == 0.1793740787340172;
@@ -97,7 +98,8 @@ version(mir_stat_test)
 @safe pure nothrow @nogc
 unittest
 {
-    import mir.test;
+    import mir.test: shouldApprox;
+
     gevCDF(-3, 2, 3, -0.5).shouldApprox == 0.034696685646156494;
     gevCDF(-1, 2, 3, +0.5).shouldApprox == 0.01831563888873418;
     gevCDF(-1, 2, 3, 0.0).shouldApprox == 0.06598803584531254;
@@ -111,6 +113,48 @@ unittest
     import mir.test: should;
     gevCDF(-1.0, 0, 1, 1).should == 0;
     gevCDF(1.0, 0, 1, -1).should == 1;
+}
+
+/++
+Computes the generalized extreme value (GEV) complementary cumulatve distribution function (CCDF).
+
+Params:
+    x = value to evaluate
+    mu = location
+    sigma = scale
+    xi = shape
+
+See_also:
+    $(LINK2 https://en.wikipedia.org/wiki/Generalized_extreme_value_distribution, Generalized Extreme Value (GEV) Distribution)
++/
+T gevCCDF(T)(const T x, const T mu, const T sigma, const T xi)
+    if (isFloatingPoint!T)
+    in (xi >= 0 || x <= mu - sigma / xi, "if xi is less than zero, x must be less than or equal to mu - sigma / xi")
+    in (xi <= 0 || x >= mu - sigma / xi, "if xi is greater than zero, xi must be greater than or equal to mu - sigma / xi")
+{
+    return 1 - gevCDF(x, mu, sigma, xi);
+}
+
+///
+version(mir_stat_test)
+@safe pure nothrow @nogc
+unittest
+{
+    import mir.test: shouldApprox;
+
+    gevCCDF(-3, 2, 3, -0.5).shouldApprox == 0.965303314353844;
+    gevCCDF(-1, 2, 3, +0.5).shouldApprox == 0.981684361111266;
+    gevCCDF(-1, 2, 3, 0.0).shouldApprox == 0.934011964154687;
+}
+
+// Checking v <= 0 branch
+version(mir_stat_test)
+@safe pure nothrow @nogc
+unittest
+{
+    import mir.test: should;
+    gevCCDF(-1.0, 0, 1, 1).should == 1;
+    gevCCDF(1.0, 0, 1, -1).should == 0;
 }
 
 /++
@@ -141,8 +185,62 @@ version(mir_stat_test)
 @safe pure nothrow @nogc
 unittest
 {
-    import mir.test;
+    import mir.test: shouldApprox;
+
     gevInvCDF(0.034696685646156494, 2, 3, -0.5).shouldApprox == -3;
     gevInvCDF(0.01831563888873418, 2, 3, +0.5).shouldApprox == -1;
     gevInvCDF(0.06598803584531254, 2, 3, 0.0).shouldApprox == -1;
+}
+
+/++
+Computes the generalized extreme value (GEV) log probability density function (LPDF).
+
+Params:
+    x = value to evaluate
+    mu = location
+    sigma = scale
+    xi = shape
+
+See_also:
+    $(LINK2 https://en.wikipedia.org/wiki/Generalized_extreme_value_distribution, Generalized Extreme Value (GEV) Distribution)
++/
+T gevLPDF(T)(const T x, const T mu, const T sigma, const T xi)
+    if (isFloatingPoint!T)
+    in (xi >= 0 || x <= mu - sigma / xi, "if xi is less than zero, x must be less than or equal to mu - sigma / xi")
+    in (xi <= 0 || x >= mu - sigma / xi, "if xi is greater than zero, xi must be greater than or equal to mu - sigma / xi")
+{
+    import mir.math.common: log;
+
+    auto s = (x - mu) / sigma;
+    if (xi.fabs <= T.min_normal)
+    {
+        auto t = exp(-s);
+        return log(t) - t;
+    }
+    auto v = 1 + xi * s;
+    if (v <= 0)
+        return -double.infinity;
+    auto a = pow(v, -1 / xi);
+    return log(a) - a - log(v * sigma);
+}
+
+///
+version(mir_stat_test)
+@safe pure nothrow @nogc
+unittest
+{
+    import mir.test: shouldApprox;
+
+    gevLPDF(-3, 2, 3, -0.5).shouldApprox == -3.85358759620891;
+    gevLPDF(-1, 2, 3, +0.5).shouldApprox == -3.01917074698827;
+    gevLPDF(-1, 2, 3, 0.0).shouldApprox == -1.71828182845905;
+}
+
+// Checking v <= 0 branch
+version(mir_stat_test)
+@safe pure nothrow @nogc
+unittest
+{
+    import mir.test: shouldApprox;
+    gevLPDF(-1.0, 0, 1, 1).shouldApprox == -double.infinity;
 }
