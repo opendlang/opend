28bce29b90abbd18a50faadfc51b22084c4415c7
John Michael Hall <john.michael.hall@gmail.com>
2023-02-06 08:51:35 -0500

    Make students t LPDF use log1p instead of xlog1p

--- source/mir/stat/distribution/students_t.d
+++ source/mir/stat/distribution/students_t.d
@@ -486,13 +486,13 @@ T studentsTLPDF(T)(const T x, const T nu)
     in (nu > 0, "nu must be greater than zero")
 {
     import mir.math.common: log;
-    import mir.math.internal.xlogy: xlog1py;
     import mir.stat.constant: LOGPI;
     import mir.stat.distribution.normal: normalLPDF;
+    import std.math.exponential: log1p;
     import std.mathspecial: logGamma;
 
     if (nu != T.infinity) {
-        return logGamma((nu + 1) * 0.5) - logGamma(nu * 0.5) - 0.5 * (log(nu) + T(LOGPI) + xlog1py(nu + 1, (x * x) / nu));
+        return logGamma((nu + 1) * 0.5) - logGamma(nu * 0.5) - 0.5 * (log(nu) + T(LOGPI) + (nu + 1) * log1p((x * x) / nu));
     } else {
         return normalLPDF(x);
     }
