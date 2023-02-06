05a9ff55d99bc4ee91700f66ace2b96b68233a16
John Michael Hall <john.michael.hall@gmail.com>
2023-02-05 21:27:54 -0500

    Add tables to pdf/cdf/invcdf

--- source/mir/stat/distribution/invcdf.d
+++ source/mir/stat/distribution/invcdf.d
@@ -1,11 +1,50 @@
 /++
 This package publicly imports `mir.stat.distribution.*InvCDF` modules.
 
+$(BOOKTABLE ,
+    $(TR
+        $(TH Functions)
+        $(TH Description)
+    )
+    $(LEADINGROW Univariate Discrete Distributions)
+    $(TR $(TDNW $(SUBREF bernoulli, bernoulliInvCDF)) $(TD Bernoulli Inverse CDF ))
+    $(TR $(TDNW $(SUBREF binomial, binomialInvCDF)) $(TD Binomial Inverse CDF ))
+    $(TR $(TDNW $(SUBREF geometric, geometricInvCDF)) $(TD Geometric Inverse CDF ))
+    $(TR $(TDNW $(SUBREF hypergeometric, hypergeometricInvCDF)) $(TD Hypergeometric Inverse CDF ))
+    $(TR $(TDNW $(SUBREF negative_binomial, negativeBinomialInvCDF)) $(TD Negative Binomial Inverse CDF ))
+    $(TR $(TDNW $(SUBREF poisson, poissonInvCDF)) $(TD Poisson Inverse CDF ))
+    $(TR $(TDNW $(SUBREF uniform_discrete, uniformDiscreteInvCDF)) $(TD Discrete Uniform Inverse CDF ))
+    $(LEADINGROW Univariate Continuous Distributions)
+    $(TR $(TDNW $(SUBREF beta, betaInvCDF)) $(TD Beta Inverse CDF ))
+    $(TR $(TDNW $(SUBREF beta_proportion, betaProportionInvCDF)) $(TD Beta Proportion Inverse CDF ))
+    $(TR $(TDNW $(SUBREF cauchy, cauchyInvCDF)) $(TD Cauchy Inverse CDF ))
+    $(TR $(TDNW $(SUBREF chi2, chi2InvCDF)) $(TD Chi-squared Inverse CDF ))
+    $(TR $(TDNW $(SUBREF exponential, exponentialInvCDF)) $(TD Exponential Inverse CDF ))
+    $(TR $(TDNW $(SUBREF f, fInvCDF)) $(TD F Inverse CDF ))
+    $(TR $(TDNW $(SUBREF gamma, gammaInvCDF)) $(TD Gamma Inverse CDF ))
+    $(TR $(TDNW $(SUBREF generalized_pareto, generalizedParetoInvCDF)) $(TD Generalized Pareto Inverse CDF ))
+    $(TR $(TDNW $(SUBREF gev, gevInvCDF)) $(TD Generalized Extreme Value (GEV) Inverse CDF ))
+    $(TR $(TDNW $(SUBREF laplace, laplaceInvCDF)) $(TD Laplace Inverse CDF ))
+    $(TR $(TDNW $(SUBREF log_normal, logNormalInvCDF)) $(TD Log-normal Inverse CDF ))
+    $(TR $(TDNW $(SUBREF logistic, logisticInvCDF)) $(TD Logistic Inverse CDF ))
+    $(TR $(TDNW $(SUBREF normal, normalInvCDF)) $(TD Normal Inverse CDF ))
+    $(TR $(TDNW $(SUBREF pareto, paretoInvCDF)) $(TD Pareto Inverse CDF ))
+    $(TR $(TDNW $(SUBREF rayleigh, rayleighInvCDF)) $(TD Rayleigh Inverse CDF ))
+    $(TR $(TDNW $(SUBREF students_t, studentsTInvCDF)) $(TD Student's t Inverse CDF ))
+    $(TR $(TDNW $(SUBREF uniform, uniformInvCDF)) $(TD Continuous Uniform Inverse CDF ))
+    $(TR $(TDNW $(SUBREF weibull, weibullInvCDF)) $(TD Weibull Inverse CDF ))
+    $(LEADINGROW Multivariate Distributions)
+    $(TR $(TDNW $(SUBREF categorical, categoricalInvCDF)) $(TD Categorical Inverse CDF ))
+)
+
 License: $(HTTP www.apache.org/licenses/LICENSE-2.0, Apache-2.0)
 
 Authors: John Michael Hall, Ilya Yaroshenko
 
 Copyright: 2022-3 Mir Stat Authors.
+
+Macros:
+SUBREF = $(REF_ALTTEXT $(TT $2), $2, mir, stat, distribution, $1)$(NBSP)
 
 +/
 
