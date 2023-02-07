2801291f65640b844f248f4918b6eb24aee048e6
John Michael Hall <john.michael.hall@gmail.com>
2023-02-06 20:07:32 -0500

    Fixup spacing

--- source/mir/stat/constant.d
+++ source/mir/stat/constant.d
@@ -21,7 +21,7 @@ enum real LOGSQRT2PI = 0x0.eb3f8e4325f5a53494bc900144192p+0L;  // log(sqrt(2pi))
 ///
 enum real SQRTPI =     0x1.c5bf891b4ef6aa79c3b0520d5db93p+0L; // sqrt(PI);
 ///
-enum real M_SQRTPI =  0x0.906eba8214db688d71d48a7f6bfec3p+0L; // 1/sqrt(pi)
+enum real M_SQRTPI =   0x0.906eba8214db688d71d48a7f6bfec3p+0L; // 1/sqrt(pi)
 
 
 
