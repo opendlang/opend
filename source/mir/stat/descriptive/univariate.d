ebc91569f6d213602b877a9c49f2a5b88796cedd
John Michael Hall <john.michael.hall@gmail.com>
2023-02-12 17:41:45 -0500

    Fixup

--- source/mir/stat/descriptive/univariate.d
+++ source/mir/stat/descriptive/univariate.d
@@ -67,7 +67,7 @@ Copyright: 2022 Mir Stat Authors.
 Macros:
 SUBREF = $(REF_ALTTEXT $(TT $2), $2, mir, stat, $1)$(NBSP)
 MATHREF = $(GREF_ALTTEXT mir-algorithm, $(TT $2), $2, mir, math, $1)$(NBSP)
-MATHREF_ALT = $(GREF_ALTTEXT mir-algorithm, $($(TT $2)), $2, mir, math, $1)$(NBSP)
+MATHREF_ALT = $(GREF_ALTTEXT mir-algorithm, $(B $(TT $2)), $2, mir, math, $1)$(NBSP)
 NDSLICEREF = $(GREF_ALTTEXT mir-algorithm, $(TT $2), $2, mir, ndslice, $1)$(NBSP)
 T2=$(TR $(TDNW $(LREF $1)) $(TD $+))
 T3=$(TR $(TDNW $(LREF $1)) $(TD $2) $(TD $3))
