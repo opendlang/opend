8ddb1f1756c6926e25b7139b0e170abd8dcaee6a
John Michael Hall <john.michael.hall@gmail.com>
2023-03-10 20:40:06 -0500

    Update categorical for enhanced sum (that handles const slices)

--- source/mir/stat/distribution/categorical.d
+++ source/mir/stat/distribution/categorical.d
@@ -14,28 +14,9 @@ module mir.stat.distribution.categorical;
 import mir.algorithm.iteration: all;
 import mir.internal.utility: isFloatingPoint;
 import mir.math.common: approxEqual;
+import mir.math.sum: elementType, sumType, sum;
 import mir.ndslice.slice: Slice, SliceKind;
-
-private
-@safe pure nothrow @nogc
-T sum(T, SliceKind kind)(scope const Slice!(const(T)*, 1, kind) p)
-{
-    import std.traits: Unqual;
-
-    Unqual!T output = 0;
-    foreach (e; p) {
-        output += e;
-    }
-    return output;
-}
-
-private
-@safe pure nothrow @nogc
-T sum(T)(scope const T[] p...)
-{
-    import mir.math.sum: sum;
-    return p.sum;
-}
+import std.traits: CommonType;
 
 /++
 Computes the Categorical probability mass function (PMF).
@@ -48,8 +29,8 @@ See_also:
     $(LINK2 https://en.wikipedia.org/wiki/Categorical_distribution, Categorical Distribution)
 +/
 @safe pure nothrow @nogc
-T categoricalPMF(T, SliceKind kind)(const size_t x, scope const Slice!(const(T)*, 1, kind) p)
-    if (isFloatingPoint!T)
+elementType!(Slice!(Iterator, 1, kind)) categoricalPMF(Iterator, SliceKind kind)(const size_t x, scope const Slice!(Iterator, 1, kind) p)
+    if (isFloatingPoint!(elementType!(Slice!(Iterator, 1, kind))))
     in (x < p.length, "x must be less than the length of p")
     in (p.sum.approxEqual(1.0), "p must sum to 1")
     in (p.all!("a >= 0"), "p must be greater than or equal to 0")
@@ -100,6 +81,23 @@ unittest
     2.categoricalPMF(p).shouldApprox == 0.4;
 }
 
+///
+version(mir_stat_test)
+@safe pure nothrow @nogc
+unittest
+{
+    import mir.ndslice.slice: sliced;
+    import mir.test: shouldApprox;
+
+    static immutable x = [1.0, 5, 4];
+    auto p = x.sliced;
+    auto q = p / sum(p);
+
+    0.categoricalPMF(q).shouldApprox == 0.1;
+    1.categoricalPMF(q).shouldApprox == 0.5;
+    2.categoricalPMF(q).shouldApprox == 0.4;
+}
+
 /++
 Computes the Categorical cumulative distribution function (CDF).
 
@@ -111,8 +109,8 @@ See_also:
     $(LINK2 https://en.wikipedia.org/wiki/Categorical_distribution, Categorical Distribution)
 +/
 @safe pure nothrow @nogc
-T categoricalCDF(T, SliceKind kind)(const size_t x, scope const Slice!(const(T)*, 1, kind) p)
-    if (isFloatingPoint!T)
+sumType!(Slice!(Iterator, 1, kind)) categoricalCDF(Iterator, SliceKind kind)(const size_t x, scope const Slice!(Iterator, 1, kind) p)
+    if (isFloatingPoint!(elementType!(Slice!(Iterator, 1, kind))))
     in (x < p.length, "x must be less than the length of p")
     in (p.sum.approxEqual(1.0), "p must sum to 1")
     in (p.all!("a >= 0"), "p must be greater than or equal to 0")
@@ -174,8 +172,8 @@ See_also:
     $(LINK2 https://en.wikipedia.org/wiki/Categorical_distribution, Categorical Distribution)
 +/
 @safe pure nothrow @nogc
-T categoricalCCDF(T, SliceKind kind)(const size_t x, scope const Slice!(const(T)*, 1, kind) p)
-    if (isFloatingPoint!T)
+sumType!(Slice!(Iterator, 1, kind)) categoricalCCDF(Iterator, SliceKind kind)(const size_t x, scope const Slice!(Iterator, 1, kind) p)
+    if (isFloatingPoint!(elementType!(Slice!(Iterator, 1, kind))))
     in (x < p.length, "x must be less than the length of p")
     in (p.sum.approxEqual(1.0), "p must sum to 1")
     in (p.all!("a >= 0"), "p must be greater than or equal to 0")
@@ -237,17 +235,15 @@ See_also:
     $(LINK2 https://en.wikipedia.org/wiki/Categorical_distribution, Categorical Distribution)
 +/
 @safe pure nothrow @nogc
-size_t categoricalInvCDF(T, SliceKind kind)(const T q, scope const Slice!(const(T)*, 1, kind) p)
-    if (isFloatingPoint!T)
+size_t categoricalInvCDF(T, Iterator, SliceKind kind)(const T q, scope const Slice!(Iterator, 1, kind) p)
+    if (isFloatingPoint!(CommonType!(T, elementType!(Slice!(Iterator, 1, kind)))))
     in (q >= 0, "q must be greater than or equal to 0")
     in (q <= 1, "q must be less than or equal to 1")
     in (p.sum.approxEqual(1.0), "p must sum to 1")
     in (p.all!("a >= 0"), "p must be greater than or equal to 0")
     in (p.all!("a <= 1"), "p must be less than or equal to 1")
 {
-    import std.traits: Unqual;
-
-    Unqual!T s = 0.0;
+    CommonType!(T, elementType!(typeof(p))) s = 0.0;
     size_t i;
     s += p[i];
     while (q > s) {
@@ -267,9 +263,7 @@ size_t categoricalInvCDF(T)(const T q, scope const T[] p...)
     in (p.all!("a >= 0"), "p must be greater than or equal to 0")
     in (p.all!("a <= 1"), "p must be less than or equal to 1")
 {
-    import std.traits: Unqual;
-
-    Unqual!T s = 0.0;
+    T s = 0.0;
     size_t i;
     s += p[i];
     while (q > s) {
@@ -326,8 +320,8 @@ See_also:
     $(LINK2 https://en.wikipedia.org/wiki/Categorical_distribution, Categorical Distribution)
 +/
 @safe pure nothrow @nogc
-T categoricalLPMF(T, SliceKind kind)(const size_t x, scope const Slice!(const(T)*, 1, kind) p)
-    if (isFloatingPoint!T)
+elementType!(Slice!(Iterator, 1, kind)) categoricalLPMF(Iterator, SliceKind kind)(const size_t x, scope const Slice!(Iterator, 1, kind) p)
+    if (isFloatingPoint!(elementType!(Slice!(Iterator, 1, kind))))
     in (x < p.length, "x must be less than the length of p")
     in (p.sum.approxEqual(1.0), "p must sum to 1")
     in (p.all!("a >= 0"), "p must be greater than or equal to 0")
