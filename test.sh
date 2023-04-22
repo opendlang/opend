e5b22c5946081d453069a820155a47720afabc88
Guillaume Piolat <contact@auburnsounds.com>
2023-04-22 12:23:35 +0200

    Fix macos tests

--- test.sh
+++ test.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
+dub test --compiler ldc2 -a x86_64 -b unittest-release-below-avx -f
+dub test --compiler ldc2 -a x86_64 -b unittest-below-avx -f
+dub test --compiler ldc2 -a arm64-apple-macos -f
+dub test --compiler ldc2 -a x86_64 -f
+dub test --compiler ldc2 -a arm64-apple-macos -b unittest-inst -f
+dub test --compiler ldc2 -a arm64-apple-macos -b unittest-release -f
+dub test --compiler ldc2 -a x86_64 -b unittest-release -f
+dub test --compiler ldc2 -a arm64-apple-macos -b unittest-release-inst -f
 
 # Note: not sure why, LLVM error when using "inst" configurations in Rosetta (-mcpu=native)
-dub test --compiler ldc2 -a x86_64 -b unittest-release-rosetta -f
-dub test --compiler ldc2 -a x86_64 -b unittest-rosetta -f
-
-#dub test --compiler ldc2 -a arm64-apple-macos -f
-#dub test --compiler ldc2 -a x86_64 -f
-#dub test --compiler ldc2 -a arm64-apple-macos -b unittest-inst -f
-
-#dub test --compiler ldc2 -a arm64-apple-macos -b unittest-release -f
-#dub test --compiler ldc2 -a x86_64 -b unittest-release -f
-#dub test --compiler ldc2 -a arm64-apple-macos -b unittest-release-inst -f
-
-
-
+# hence the below-avx configurations
