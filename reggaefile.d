899788bc487cc5d64ec9b7ed3ef047df031712ed
Atila Neves <atila.neves@gmail.com>
2023-11-08 17:18:03 +0100

    Update reggaefile

--- reggaefile.d
+++ reggaefile.d
@@ -1,19 +1,13 @@
 import reggae;
 import std.typecons;
 
-enum debugFlags = "-w -g -debug";
-
-alias lib = dubDefaultTarget!(CompilerFlags(debugFlags));
-alias ut = dubTestTarget!(CompilerFlags(debugFlags ~ " -cov"));
-alias utl = dubConfigurationTarget!(
-    Configuration("utl"),
-    CompilerFlags(debugFlags ~ " -unittest -version=unitThreadedLight -cov")
-);
+alias lib = dubTarget!();
+alias ut = dubTestTarget!(CompilationMode.options, Yes.coverage);
 alias asan = dubConfigurationTarget!(
     Configuration("asan"),
-    CompilerFlags(debugFlags ~ " -unittest -cov -fsanitize=address"),
+    CompilerFlags("-unittest -cov -fsanitize=address"),
     LinkerFlags("-fsanitize=address"),
 );
 
 
-mixin build!(lib, optional!ut, optional!utl, optional!asan);
+mixin build!(lib, optional!ut, optional!asan);
