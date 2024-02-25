1d0f957932626892ae2ee72b8ae5708aa07d0417
Igor Deepak <IgorDeepakM@noreply.com>
2024-02-25 01:08:24 +0100

    Add OS memory API

--- druntime/src/rt/sys/windows/config.d
+++ druntime/src/rt/sys/windows/config.d
@@ -7,3 +7,14 @@ enum string osMutexImport = "rt.sys.windows.osmutex";
 enum string osSemaphoreImport = "rt.sys.windows.ossemaphore";
 enum string osConditionImport = "rt.sys.windows.oscondition";
 enum string osEventImport = "rt.sys.windows.osevent";
+enum string osMemoryImport = "rt.sys.windows.osmemory";
+
+
+/**
+* Indicates if an implementation supports fork().
+*
+* The value shown here is just demostrative, the real value is defined based
+* on the OS it's being compiled in.
+* enum HaveFork = true;
+*/
+enum HaveFork = false;
