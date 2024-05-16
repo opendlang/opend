07ac65ac007a5cbe2e6061b7ed2f8480e2a47866
Adam D. Ruppe <destructionator@gmail.com>
2024-05-16 08:14:48 -0400

    allow == on classes since it works anyway, this an obsolete error

--- compiler/src/dmd/opover.d
+++ compiler/src/dmd/opover.d
@@ -930,6 +930,7 @@ Expression op_overload(Expression e, Scope* sc, EXP* pop = null)
                 return null;
             }
 
+	    /+
             /* Check for class equality with null literal or typeof(null).
              */
             if (t1.ty == Tclass && e.e2.op == EXP.null_ ||
@@ -940,6 +941,7 @@ Expression op_overload(Expression e, Scope* sc, EXP* pop = null)
                     EXPtoString(e.op).ptr);
                 return ErrorExp.get();
             }
+	    +/
             if (t1.ty == Tclass && t2.ty == Tnull ||
                 t1.ty == Tnull && t2.ty == Tclass)
             {
