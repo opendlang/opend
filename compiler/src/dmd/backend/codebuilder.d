918f7444fb95d3ff9ddcd8a3feb8a2dade3851db
Adam D. Ruppe <destructionator@gmail.com>
2026-07-26 09:18:15 -0400

    null deref fix

--- compiler/src/dmd/backend/codebuilder.d
+++ compiler/src/dmd/backend/codebuilder.d
@@ -373,10 +373,7 @@ struct CodeBuilder
     @trusted
     code *last()
     {
-        // g++ and clang++ complain about offsetof() because of the code::code() constructor.
-        // return (code *)((char *)pTail - offsetof(code, next));
-        // So do our own.
-        return cast(code *)(cast(void *)pTail - (cast(void*)&(*pTail).next - cast(void*)*pTail));
+        return cast(code*)(cast(void*)pTail - code.next.offsetof);
     }
 
     /*************************************
