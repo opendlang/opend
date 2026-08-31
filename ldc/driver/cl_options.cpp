8c7221c2988abd27d810eb76e1182c7cd9869c7c
Adam D. Ruppe <destructionator@gmail.com>
2026-08-31 07:24:59 -0400

    actually fix

--- ldc/driver/cl_options.cpp
+++ ldc/driver/cl_options.cpp
@@ -518,7 +518,7 @@ static cl::opt<bool, true>
             cl::ValueDisallowed);
 
 static cl::opt<bool, true>
-    ludicrous("ludicrous", cl::ZeroOrMore, cl::location(global.params.ludicrous),
+    ludicrous("ludicrous", cl::ZeroOrMore, cl::location(global.params.release),
             cl::desc("Compile ludicrous version, defaulting to disabled "
                      "asserts/contracts/invariants, and bounds checks in @safe "
                      "functions only"),
